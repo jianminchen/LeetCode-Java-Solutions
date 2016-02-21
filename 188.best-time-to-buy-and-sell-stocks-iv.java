bfd28158c61b40fdbf85e8c62374e78eb415a56d
Dawei Li <douglasleer@gmail.com>
2016-02-21 17:27:16 -0500

    Uploading new solutions

---
+++ 188.best-time-to-buy-and-sell-stocks-iv.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length <= 1) return 0;
+        int n = prices.length;
+        if (k >= n / 2) {
+            int maxProfit = 0;
+            for (int i = 1; i < n; ++i) {
+                if (prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
+            }
+            return maxProfit;
+        }
+        
+        int[][] profit = new int[k + 1][prices.length];
+        // profit i, j is the maxprofit can be obtained by conducting i transactions up to prices[j];
+        // profit[0][j] = 0; profit[i][0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            int locMax = profit[i - 1][0] - prices[0];
+            for (int j = 1; j < n; ++j) {
+                profit[i][j] = Math.max(profit[i][j - 1], prices[j] + locMax);
+                locMax = Math.max(locMax, profit[i - 1][j] - prices[j]);
+            }
+        }
+        return profit[k][n - 1];
+    }
+}
---
+++ 282.expression-add-operators.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        String path = "";
+        helper(res, path, num, target, 0, 0, 0);
+        return res;
+    }
+    public void helper(List<String> res, String path, String num, int target, int pos, long eval, long multed) {
+        if (pos == num.length()) {
+            if (target == eval) {
+                res.add(path);
+            }
+            return; // reach at the end.
+        }
+        for (int i = pos; i < num.length(); ++i) {
+            if (i != pos && num.charAt(pos) == '0') break; // a greater-than 2-digit number does not start with 0
+            // if we do not consider integer overflow: java.lang.NumberFormatException: For input string: "3456237490"
+            // int cur = Integer.parseInt(num.substring(pos, i + 1));
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+            if (pos == 0) {
+                // initialize. such that multed will not be zero.
+                helper(res, path + num.substring(pos, i + 1), num, target, i + 1, cur, cur); 
+            }
+            else {
+                helper(res, path + "+" + cur, num, target, i + 1, eval + cur, cur);
+                helper(res, path + "-" + cur, num, target, i + 1, eval - cur, -cur);
+                helper(res, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur);
+            }
+            
+        }
+    }
+}
