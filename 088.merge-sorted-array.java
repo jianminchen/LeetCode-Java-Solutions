e7080af6c89da4217cd9d1348cf98657f1e87e63
douglasleer <douglasleer@gmail.com>
2016-02-26 18:48:34 -0500

    Update 088.merge-sorted-array.java

--- 088.merge-sorted-array.java
+++ 088.merge-sorted-array.java
@@ -3,19 +3,18 @@
  */
 
 public class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index = m + n - 1;
-        int i = m - 1, j = n - 1;
-        
-        while (i >= 0 || j >= 0) {
-            if (i < 0 || (i >= 0 && j >= 0 && nums1[i] < nums2[j])) {
-                nums1[index] = nums2[j--];
-                
-	    }
-            else {
-                nums1[index] = nums1[i--];
-            }
-            --index;
-        }
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    if (nums1 == null || nums2 == null) {
+      throw new NullPointerException();
     }
+    int fillIndex = m + n - 1;
+    int i = m - 1, j = n - 1;
+    while (fillIndex >= 0) {
+      if (i < 0 || (j >= 0 && nums1[i] < nums2[j])) {
+        nums1[fillIndex--] = nums2[j--];
+      } else {
+        nums1[fillIndex--] = nums1[i--];
+      }
+    }
+  }
 }
