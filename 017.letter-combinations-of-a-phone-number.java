8c91495a4ccb30045a00490c64f664abbce79f90
douglasleer <douglasleer@gmail.com>
2016-02-26 18:58:20 -0500

    Update 017.letter-combinations-of-a-phone-number.java

--- 017.letter-combinations-of-a-phone-number.java
+++ 017.letter-combinations-of-a-phone-number.java
@@ -3,37 +3,27 @@
  */
 
 public class Solution {
-    public List<String> letterCombinations(String digits) {
-        
-        String[] chart = new String[10];
-        chart[0] = " ";
-        chart[1] = "";
-        chart[2] = "abc";
-        chart[3] = "def";
-        chart[4] = "ghi";
-        chart[5] = "jkl";
-        chart[6] = "mno";
-        chart[7] = "pqrs";
-        chart[8] = "tuv";
-        chart[9] = "wxyz";
-        List<String> res = new ArrayList<String>();
-        if (digits.length() == 0) return res;
-        for (int i = 0; i < digits.length(); ++i) {
-            if (digits.charAt(i) == '1' || digits.charAt(i) == '0') return res; 
-        }
-        res.add("");
-        for (int i = 0; i < digits.length(); ++i) {
-            List<String> nextList = new ArrayList<String>();
-            String toAppend = chart[digits.charAt(i) - '0'];
-            for (String aStr : res) {
-                for (int j = 0; j < toAppend.length(); ++j) {
-                    String newStr = aStr + toAppend.charAt(j);
-                    nextList.add(newStr);
-                }
-            }
-            res = nextList;
-        }
-
-        return res;
+  public List<String> letterCombinations(String digits) {
+    if (digits == null) {
+      throw new NullPointerException();
     }
+    List<String> res = new ArrayList<String>();
+    if (digits.length() == 0) {
+      return res;
+    }
+    String[] chart = {" ", "", "abc", "def", "ghi", "jkl", "mno", 
+        "pqrs", "tuv", "wxyz"};
+    res.add("");
+    for (int i = 0; i < digits.length(); ++i) {
+      List<String> nextList = new ArrayList<String>();
+      for (String s : res) {
+        for (int j = 0; j < chart[digits.charAt(i) - '0'].length(); ++j) {
+          String newS = s + chart[digits.charAt(i) - '0'].charAt(j);
+          nextList.add(newS);
+        }
+      }
+      res = nextList;
+    }
+    return res;
+  }
 }
