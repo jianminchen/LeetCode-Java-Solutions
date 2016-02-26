3d132fc8d2230e72f03d9e6542d271361c05a4e9
douglasleer <douglasleer@gmail.com>
2016-02-26 18:55:28 -0500

    Update 173.binary-search-tree-iterator.java

--- 173.binary-search-tree-iterator.java
+++ 173.binary-search-tree-iterator.java
@@ -5,46 +5,43 @@
 /**
  * Definition for binary tree
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *   int val;
+ *   TreeNode left;
+ *   TreeNode right;
+ *   TreeNode(int x) { val = x; }
  * }
  */
 
 public class BSTIterator {
-    Stack<TreeNode> stk;
-    // TreeNode cur;
-    public BSTIterator(TreeNode root) {
-        stk = new Stack<TreeNode>();
-        TreeNode cur = root;
-        while (cur != null) {
-            stk.push(cur);
-            cur = cur.left;
-        }
+  private Stack<TreeNode> stk;
+  
+  public BSTIterator(TreeNode root) {
+    stk = new Stack<TreeNode>();
+    TreeNode cur = root;
+    while (cur != null) {
+      stk.push(cur);
+      cur = cur.left;
     }
+  }
 
-    /** @return whether we have a next smallest number */
-    public boolean hasNext() {
-        return !stk.isEmpty();
-    }
+  /** @return whether we have a next smallest number */
+  public boolean hasNext() {
+    return !stk.isEmpty();
+  }
 
-    /** @return the next smallest number */
-    public int next() {
-        TreeNode cur = stk.pop();
-        int returnValue = cur.val;
-        if (cur.right != null) {
-            cur = cur.right;
-            while (cur != null) {
-                stk.push(cur);
-                cur = cur.left;
-            }
-        }
-        else {
-            ;
-        }
-        return returnValue;
+  /** @return the next smallest number */
+  public int next() {
+    TreeNode cur = stk.pop();
+    int value= cur.val;
+    if (cur.right != null) {
+      cur = cur.right;
+      while (cur != null) {
+        stk.push(cur);
+        cur = cur.left;
+      }
     }
+    return value;
+  }
 }
 
 /**
