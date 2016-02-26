db449cd214c38118757607569b022a37a3150bf1
douglasleer <douglasleer@gmail.com>
2016-02-26 18:59:18 -0500

    Update 120.triangle.java

--- 120.triangle.java
+++ 120.triangle.java
@@ -3,17 +3,23 @@
  */
 
 public class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle == null || triangle.size() == 0) return 0;
-        int[] min = new int[triangle.size()];
-        for (int i = 0; i < min.length; ++i) {
-            min[i] = triangle.get(triangle.size() - 1).get(i);
-        }
-        for (int i = triangle.size() - 2; i >= 0; --i) {
-            for (int j = 0; j <= i; ++j) {
-                min[j] = Math.min(min[j], min[j + 1]) + triangle.get(i).get(j);
-            }
-        }
-        return min[0];
+  public int minimumTotal(List<List<Integer>> triangle) {
+    if (triangle == null) {
+      throw new NullPointerException();
     }
+    if (triangle.size() == 0) {
+      return 0;
+    }
+    int[] minFromBot = new int[triangle.size()];
+    for (int i = 0; i < minFromBot.length; ++i) { // initialize
+      minFromBot[i] = triangle.get(triangle.size() - 1).get(i);
+    }
+    for (int row = triangle.size() - 2; row >= 0; --row) {
+      for (int col = 0; col <= row; ++col) {
+        minFromBot[col] = Math.min(minFromBot[col], minFromBot[col + 1])
+            + triangle.get(row).get(col);
+      }
+    }
+    return minFromBot[0];
+  }
 }
