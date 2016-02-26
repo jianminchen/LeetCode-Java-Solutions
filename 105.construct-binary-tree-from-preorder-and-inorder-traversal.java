b22a7907f1d4a6b427ff7fac9422a7820f461cdf
douglasleer <douglasleer@gmail.com>
2016-02-26 18:52:46 -0500

    Update 105.construct-binary-tree-from-preorder-and-inorder-traversal.java

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -5,27 +5,37 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *   int val;
+ *   TreeNode left;
+ *   TreeNode right;
+ *   TreeNode(int x) { val = x; }
  * }
  */
 public class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || preorder.length == 0) return null;
-        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    if (preorder == null || inorder == null) {
+      return null;
     }
-    public TreeNode buildTree(int[] preorder, int pfrom, int pto, int[] inorder, int ifrom, int ito) {
-        if (pfrom > pto) return null;
-        if (pfrom == pto) return new TreeNode(preorder[pfrom]);
-        TreeNode root = new TreeNode(preorder[pfrom]);
-        int i = ifrom;
-        for (; i <= ito; ++i) {
-            if (inorder[i] == preorder[pfrom]) break;
-        }
-        root.left = buildTree(preorder, pfrom + 1, pfrom + i - ifrom, inorder, ifrom, i - 1);
-        root.right = buildTree(preorder, pfrom + 1 + i - ifrom, pto,  inorder, i + 1, ito);
-        return root;
+    return buildTree(preorder, 0, preorder.length - 1, 
+        inorder, 0, inorder.length - 1);
+  }
+
+  private TreeNode buildTree(int[] preorder, int pfrom, int pto, 
+      int[] inorder, int ifrom, int ito) {
+    if (pfrom > pto) {
+      return null;
     }
+    TreeNode root = new TreeNode(preorder[pfrom]);
+    int i = ifrom;
+    for (; i <= ito; ++i) {
+      if (inorder[i] == preorder[pfrom]) {
+        break;
+      }
+    }
+    root.left = buildTree(preorder, pfrom + 1, pfrom + i - ifrom, 
+        inorder, ifrom, i - 1);
+    root.right = buildTree(preorder, pfrom + 1 + i - ifrom, pto,  
+        inorder, i + 1, ito);
+    return root;
+  }
 }
