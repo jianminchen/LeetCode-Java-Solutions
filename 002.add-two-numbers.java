d9a2b08b0b6d85c34f2bc2c437362da6583380c0
douglasleer <douglasleer@gmail.com>
2016-02-26 20:06:40 -0500

    Update 002.add-two-numbers.java

--- 002.add-two-numbers.java
+++ 002.add-two-numbers.java
@@ -10,31 +10,26 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carryIn = 0;
-        ListNode cur1 = l1;
-        ListNode cur2 = l2;
-        ListNode dummy = new ListNode(0);
-        ListNode prev = dummy;
+        ListNode cur1 = l1, cur2 = l2;
+        ListNode dummy = new ListNode(0), prev = dummy;
         while (cur1 != null || cur2 != null) {
-            int num1 = 0;
-            int num2 = 0;
-            if (cur1 != null) num1 = cur1.val;
-            if (cur2 != null) num2 = cur2.val;
+            int num1 = cur1 == null ? 0 : cur1.val;
+            int num2 = cur2 == null ? 0 : cur2.val;
             int sum = carryIn + num1 + num2;
             int digit = sum % 10;
             carryIn = sum / 10;
-            ListNode newNode = new ListNode(digit);
-            prev.next = newNode;
+            prev.next =  new ListNode(digit);
             prev = prev.next;
             if (cur1 != null) cur1 = cur1.next;
             if (cur2 != null) cur2 = cur2.next;
         }
         if (carryIn != 0) {
-            ListNode lastNode = new ListNode(carryIn);
-            prev.next = lastNode;
+            prev.next =  new ListNode(carryIn);;
         }
-        return dummy.next;        
+        return dummy.next;
     }
 }
