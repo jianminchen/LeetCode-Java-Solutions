79c7261815d94520a651ab934747d4a6bde64f7a
douglasleer <douglasleer@gmail.com>
2016-02-27 01:22:04 -0500

    Update 012.integer-to-roman.java

--- 012.integer-to-roman.java
+++ 012.integer-to-roman.java
@@ -4,48 +4,39 @@
 
 public class Solution {
     public String intToRoman(int num) {
-        char[][] chart= new char[4][2];
-        chart[0][0] = 'I';
-        chart[0][1] = 'V';
-        chart[1][0] = 'X';
-        chart[1][1] = 'L';
-        chart[2][0] = 'C';
-        chart[2][1] = 'D';
-        chart[3][0] = 'M';
+        char[][] chart = {{'I', 'V'}, {'X', 'L'}, {'C', 'D'}, {'M', ' '}};
         
-        int whichDigit = 0;
+        int which = 0;
         String s = new String();
         while (num != 0) {
             int digit = num % 10;
-            // only consider the case when digit != 0. if 0, go to the next digit directly
-            if (digit != 0) {
-                StringBuilder sb = new StringBuilder();
-                if (digit <= 3) {
+            StringBuilder sb = new StringBuilder();
+            switch (digit) {
+                case 1: case 2: case 3:
                     for (int i = 1; i <= digit; ++i) {
-                        sb.append(chart[whichDigit][0]);
+                        sb.append(chart[which][0]);
                     }
-                } // for the thousand bit, digit cannot be greater than 3.
-                else if (digit == 4) {
-                    sb.append(chart[whichDigit][0]);
-                    sb.append(chart[whichDigit][1]);
-                }
-                else if (digit == 5) {
-                    sb.append(chart[whichDigit][1]);
-                }
-                else if (digit <= 8) { // digit 6, 7, 8
-                    sb.append(chart[whichDigit][1]);
+                    break;
+                case 4:
+                    sb.append(chart[which][0]);
+                    sb.append(chart[which][1]);
+                    break;
+                case 5: case 6: case 7: case 8:
+                    sb.append(chart[which][1]);
                     for (int i = 6; i <= digit; ++i) {
-                        sb.append(chart[whichDigit][0]);
+                        sb.append(chart[which][0]);
                     }
-                }
-                else { // digit 9.
-                    sb.append(chart[whichDigit][0]);
-                    sb.append(chart[whichDigit+1][0]);
-                }
-                s = (new String(sb)) + s;
+                    break;
+                case 9:
+                    sb.append(chart[which][0]);
+                    sb.append(chart[which + 1][0]);
+                    break;
+                default:
+                    break;
             }
+            s = new String(sb) + s;
             num = num / 10;
-            whichDigit ++;
+            ++which;
         }
         return s;
     }
