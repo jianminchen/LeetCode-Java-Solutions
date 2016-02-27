b2115aaf754d907bf7bbf1d77c686d5bc582d089
douglasleer <douglasleer@gmail.com>
2016-02-26 19:05:44 -0500

    Update 037.sudoku-solver.java

--- 037.sudoku-solver.java
+++ 037.sudoku-solver.java
@@ -3,79 +3,62 @@
  */
 
 public class Solution {
-	public void solveSudoku(char[][] board) {
-		List<int[]> locs = locsToFill(board);
-		boolean[][][] exist = new boolean[3][board.length][board.length];
-		// let exist[0][i][j] be whether number j exist in row i.
-		// let exist[1][i][j] be whether number j exist in column i.
-		// let exist[2][j][j] be whether number j exist in grid i.
-		for (int i = 0; i < board.length; ++i) {
-			for (int j = 0; j < board.length; ++j) {
-				char c = board[i][j];
-				if (c != '.') { // c can be from '1' to '9'.
-					if (exist[0][i][c - '1'] || exist[1][j][c - '1'] ||
-							exist[2][(i / 3) * 3 + j / 3][c - '1']) return;
-					exist[0][i][c - '1'] = true;
-					exist[1][j][c - '1'] = true;
-					exist[2][(i / 3) * 3 + j / 3][c - '1'] = true;
-				}
-			}
-		}
-		boolean backtrack = false;
-		int bValue = 0;
-		Stack<Integer> stk = new Stack<>();
-		
-		for (int i = 0; i < locs.size(); ++i) {
-			int cand = -1;
-			// cand is the number to fill in the board.
-			if (backtrack == false) cand = 1;
-			else cand = bValue + 1;
-			int ci = locs.get(i)[0];
-			int cj = locs.get(i)[1];
-			while (cand < 10 && 
-					(exist[0][ci][cand - 1] || exist[1][cj][cand - 1] ||
-							exist[2][(ci / 3) * 3 + cj / 3][cand - 1]))
-				++cand;
-			if (cand > 9) {
-				// need to backtrack
-				if (stk.isEmpty()) break; // no way to backtrack
-				backtrack = true;
-				bValue = stk.pop();
-				int lasti = locs.get(i - 1)[0];
-				int lastj = locs.get(i - 1)[1];
-				exist[0][lasti][bValue - 1] = false;
-				exist[1][lastj][bValue - 1] = false;
-				exist[2][(lasti / 3) * 3 + lastj / 3][bValue - 1] = false;
-				i = i - 2;
-			}
-			else { // use the currant cand value;
-				exist[0][ci][cand - 1] = true;
-				exist[1][cj][cand - 1] = true;
-				exist[2][(ci / 3) * 3 + cj / 3][cand - 1] = true;
-				stk.push(cand);
-				backtrack = false;
-				bValue = 0;
-			}
-		}
-		// after that, fill in the numbers to the board.
-		if (locs.size() == stk.size()) {
-			for (int i = locs.size() - 1; i >= 0; --i) {
-				int ci = locs.get(i)[0];
-				int cj = locs.get(i)[1];
-				board[ci][cj] = (char) (stk.pop() + '0');
-			}
-		}
-	}
-	
-	public List<int[]> locsToFill(char[][] board) {
-		List<int[]> locs = new ArrayList<>();
-		for (int i = 0; i < board.length; ++i) {
-			for (int j = 0; j < board.length; ++j) {
-				if (board[i][j] == '.') {
-					locs.add(new int[]{i, j});
-				}
-			}
-		}
-		return locs;
-	}
+  public void solveSudoku(char[][] board) {
+    if (board == null) {
+      throw new NullPointerException();
+    }
+    if (board.length != 9 || board[0].length != 9) {
+      throw new IllegalArgumentException("Board lengths should be 9.");
+    }
+    List<int[]> emptyLocs = new ArrayList<>();
+    boolean[][][] exist = new boolean[3][board.length][board[0].length];
+    preprocess(board, emptyLocs, exist);
+
+    boolean backtrack = false;
+    int prevValue = 0;
+    for (int i = 0; i < emptyLocs.size(); ++i) {
+      int cand = backtrack ? 1 + prevValue : 1; 
+      int curRow = emptyLocs.get(i)[0], curCol = emptyLocs.get(i)[1];
+      while (cand < 10 && (exist[0][curRow][cand - 1]
+          || exist[1][curCol][cand - 1]
+          || exist[2][(curRow / 3) * 3 + curCol / 3][cand - 1])) {
+        ++cand; // seek for a valid candidate number
+      }
+      if (cand > 9) { // need to backtrack
+        if (i == 0) {
+          break; // no way to backtrack
+        }
+        backtrack = true;
+	int prevRow = emptyLocs.get(i - 1)[0];
+        int prevCol = emptyLocs.get(i - 1)[1];
+        prevValue = board[prevRow][prevCol] - '0'; // record previous value
+        exist[0][prevRow][prevValue - 1] = false;
+        exist[1][prevCol][prevValue - 1] = false;
+        exist[2][(prevRow / 3) * 3 + prevCol / 3][prevValue - 1] = false;
+        i = i - 2;
+      } else { // do not backtrack; go to next empty location
+        backtrack = false;
+        exist[0][curRow][cand - 1] = true;
+        exist[1][curCol][cand - 1] = true;
+        exist[2][(curRow / 3) * 3 + curCol / 3][cand - 1] = true;
+        board[curRow][curCol] = (char) (cand + '0');
+      }
+    }
+  }
+
+  private void preprocess(char[][] board, List<int[]> emptyLocs, 
+      boolean[][][] exist) {
+    for (int i = 0; i < board.length; ++i) {
+      for (int j = 0; j < board[0].length; ++j) {
+        char c = board[i][j];
+        if (c != '.') {
+          exist[0][i][c - '1'] = true;
+          exist[1][j][c - '1'] = true;
+          exist[2][(i / 3) * 3 + j / 3][c - '1'] = true;
+        } else {
+          emptyLocs.add(new int[]{i, j});
+        }
+      }
+    }
+  }
 }
