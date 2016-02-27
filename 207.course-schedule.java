2ae0b27e9355974aa1d3fd93c20c34162132bec5
douglasleer <douglasleer@gmail.com>
2016-02-26 19:04:16 -0500

    Update 207.course-schedule.java

--- 207.course-schedule.java
+++ 207.course-schedule.java
@@ -3,39 +3,58 @@
  */
 
 public class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Set<Integer>> dependList = getDependList(numCourses, prerequisites);
-        boolean[] courseTaken = new boolean[numCourses];
-        int taken = 0;
-        while (taken < numCourses) {
-            int toTake = 0;
-            for (int i = 0; i < numCourses; ++i) {
-                if (courseTaken[i] == false && dependList.get(i).size() == 0) {
-                    ++toTake;
-                    courseTaken[i] = true;
-                    // update other dependencies;
-                    for (int j = 0; j < numCourses; ++j) {
-                        if (dependList.get(j).contains(i)) {
-                            dependList.get(j).remove(i);
-                        }
-                    }
-                }
-            }
-            if (toTake == 0) return false;
-            taken += toTake;
-        }
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    if (numCourses < 0) {
+      throw new IllegalArgumentException("numCourses should be non-negative.");
+    }
+    if (prerequisites == null || prerequisites.length == 0) {
+      return true;
+    }
+    List<List<Integer>> dpList = getDependencyList(numCourses, prerequisites);
+    boolean[] visited = new boolean[numCourses];
+    int cur = 0;
+    while (true) { // in each iteration, a dfs is conducted
+      while (cur < visited.length && visited[cur]) {
+        ++cur; // get next unvisited node
+      }
+      if (cur == numCourses) {
+        break; // finished traversing
+      }
+      Set<Integer> curActive = new HashSet<>();
+      if (dfsHelperHasCycle(cur, dpList, curActive, visited)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private List<List<Integer>> getDependencyList(int numCourses, 
+      int[][] prerequisites) {
+    List<List<Integer>> dpList = new ArrayList<>();
+    for (int i = 0; i < numCourses; ++i) {
+      dpList.add(new ArrayList<Integer>());         
+    }
+    for (int i = 0; i < prerequisites.length; ++i) {
+      dpList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+    }
+    return dpList;
+  }
+
+  private boolean dfsHelperHasCycle(int cur, List<List<Integer>> dpList, 
+      Set<Integer> curActive, boolean[] visited) {
+    visited[cur] = true;
+    curActive.add(cur);
+    for (int nb : dpList.get(cur)) {
+      if (curActive.contains(nb)) {
+        return true; // has cycle
+      }
+    }
+    for (int nb : dpList.get(cur)) {
+      if (!visited[nb] && dfsHelperHasCycle(nb, dpList, curActive, visited)) {
         return true;
-        
+      }
     }
-    public List<Set<Integer>> getDependList(int numCourses, int[][] prerequisites) {
-        List<Set<Integer>> dependList = new ArrayList<>();
-        for (int i = 0; i <numCourses; ++i) {
-            Set<Integer> set = new HashSet<Integer>();
-            dependList.add(set);
-        }
-        for (int i = 0; i < prerequisites.length; ++i) {
-            dependList.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        }
-        return dependList;
-    }
+    curActive.remove(cur); // restore such that it will not affect other paths
+    return false;
+  }
 }
