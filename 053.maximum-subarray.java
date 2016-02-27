6251765b2e01c70dfdbabbff0c555f5f0dbf898c
douglasleer <douglasleer@gmail.com>
2016-02-27 03:26:15 -0500

    Update 053.maximum-subarray.java

--- 053.maximum-subarray.java
+++ 053.maximum-subarray.java
@@ -2,16 +2,17 @@
  * @see <a href="https://leetcode.com/problems/maximum-subarray/">Maximum Subarray</a>
  */
 
+// this is the divide and conquer solution, which is not the best solution.
+// the best solution is the dynamic programming with constant memory consumption.
 public class Solution {
     public int maxSubArray(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
         return maxSum(nums, low, high);
-        
     }
     public int maxSum(int[] nums, int low, int high) {
         if (low == high) return nums[low];
-        int mid = low + ((high - low)>>1);
+        int mid = low + ((high - low) >> 1);
         int max1 = maxSum(nums, low, mid);
         int max2 = maxSum(nums, mid + 1, high);
         int maxSum1 = nums[mid];
