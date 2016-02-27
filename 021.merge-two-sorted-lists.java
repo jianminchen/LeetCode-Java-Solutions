6c9dd5bd326a22c825200d87fc1c794f379ac9cf
douglasleer <douglasleer@gmail.com>
2016-02-27 03:35:01 -0500

    Update 021.merge-two-sorted-lists.java

--- 021.merge-two-sorted-lists.java
+++ 021.merge-two-sorted-lists.java
@@ -12,42 +12,19 @@
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-        ListNode newL = null;
-        ListNode cur1 = l1;
-        ListNode cur2 = l2;
-        if (l1.val <= l2.val) {
-            newL = l1;
-            cur1 = cur1.next;
-        }
-        else {
-            newL = l2;
-            cur2 = cur2.next;
-        }
-        ListNode cur = newL;
-        while (true) {
-            if (cur1 == null) {
-                cur.next = cur2;
-                break;
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        ListNode cur1 = l1, cur2 = l2;
+        while (cur1 != null || cur2 != null) {
+            if (cur1 == null || (cur2 != null && cur1.val > cur2.val)) {
+                prev.next = cur2;
+                cur2 = cur2.next;
+            } else {
+                prev.next = cur1;
+                cur1 = cur1.next;
             }
-            else if (cur2 == null) {
-                cur.next = cur1;
-                break;
-            }
-            else { // both not null;
-                if (cur1.val <= cur2.val) {
-                    cur.next = cur1;
-                    cur1 = cur1.next;
-                }
-                else {
-                    cur.next = cur2;
-                    cur2 = cur2.next;
-                }
-                cur = cur.next;
-            }
+            prev = prev.next;
         }
-        return newL;
-        
+        return dummy.next;
     }
 }
