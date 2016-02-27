c68a7dae9df7089fdef3abf5c42644d837009f2c
douglasleer <douglasleer@gmail.com>
2016-02-27 03:48:27 -0500

    Update 022.generate-parentheses.java

--- 022.generate-parentheses.java
+++ 022.generate-parentheses.java
@@ -4,58 +4,27 @@
 
 public class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<String>();
-        if (n <= 0) return res;
-        if (n == 1) {
-            String s = "()";
-            res.add(s);
-            return res;
-        }
-        Stack<Integer> stk = new Stack<Integer>();
-        for (int i = 0; i < n; ++i) stk.push(i);
-        while (true) {
-            String str = generateStringByStack(stk);
-            res.add(str);
-            // go forward:
-            if (stk.size() - 1 >= stk.peek() + 1 - (stk.size() - 1))
-                stk.push(stk.pop() + 1);
-
-            // backtrack:
-            else {
-                stk = backtrack(stk);
-            }
-            if (stk.isEmpty()) break;
-        }
+        List<String> res = new ArrayList<>();
+        dfsHelper(0, 0, new StringBuilder(), n, res);
         return res;
     }
-    
-    public String generateStringByStack(Stack<Integer> stk) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < stk.size() * 2; ++i) {
-            sb.append(')');
+    private void dfsHelper(int lCount, int rCount, StringBuilder path, int n, List<String> res) {
+        if (lCount < rCount || lCount > n || rCount > n) return;
+        if (lCount == rCount) {
+            if (lCount == n) {
+                res.add(new String(path));
+            } else { // only can add left parenthesis
+                path.append('(');
+                dfsHelper(lCount + 1, rCount, path, n, res);
+                path.deleteCharAt(path.length() - 1);
+            }
+        } else if (lCount > rCount) { // can add left or right
+            path.append('(');
+            dfsHelper(lCount + 1, rCount, path, n, res);
+            path.deleteCharAt(path.length() - 1);
+            path.append(')');
+            dfsHelper(lCount, rCount + 1, path, n, res);
+            path.deleteCharAt(path.length() - 1);
         }
-        ArrayList<Integer> indices = new ArrayList<Integer>();
-        while (!stk.isEmpty()) {
-            indices.add(stk.pop());
-        }
-        for (int i = indices.size() - 1; i >= 0; --i) {
-            stk.push(indices.get(i));
-            sb.setCharAt(indices.get(i), '(');
-        }
-        return new String(sb);
-    }
-    
-    public Stack<Integer> backtrack(Stack<Integer> stk) {
-        int popCount = 0;
-        while (!stk.isEmpty() && stk.size() - 1 < stk.peek() + 1 - (stk.size() - 1)) {
-                stk.pop();
-                ++popCount;
-        }
-        if (stk.isEmpty()) return stk;
-        stk.push(stk.pop() + 1);
-        for (int i = 0; i < popCount; ++i) {
-            stk.push(stk.peek() + 1);
-        }
-        return stk;
     }
 }
