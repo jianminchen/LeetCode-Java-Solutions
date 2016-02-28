76cbcf4b8e88f43cca52d2ceb4cbaa43e743b108
douglasleer <douglasleer@gmail.com>
2016-02-27 23:27:28 -0500

    Update 065.valid-number.java

--- 065.valid-number.java
+++ 065.valid-number.java
@@ -5,57 +5,56 @@
 public class Solution {
     public boolean isNumber(String s) {
         s = s.trim();
-
         if (s.length() == 0) return false;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == ' ') return false;
-        }
-        int dotLoc = -1;
-        int eLoc = -1;
-        int dotCount = 0;
-        int eCount = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == '.') {
-                ++dotCount;
-                dotLoc = i;
-            }
+        int i = 0;
+        int ePos = -1;
+        for (; i < s.length(); ++i) {
             if (s.charAt(i) == 'e') {
-                ++eCount;
-                eLoc = i;
+                ePos = i;
+                break;
             }
         }
-        if (dotCount > 1 || eCount > 1) return false;
-        if (dotCount == 0 && eCount == 0) return isInteger(s);
-        else if (dotCount == 0 && eCount == 1) {
-            if (isInteger(s.substring(0, eLoc)) && isInteger(s.substring(eLoc + 1)))
-                return true;
-        }
-        else if (eCount == 0 && dotCount == 1) {
-            if ((isInteger(s.substring(0, dotLoc)) && 
-                (s.substring(dotLoc + 1).equals("") || isIntegerWithoutSymbol(s.substring(dotLoc + 1)))) ||
-                ((s.substring(0, dotLoc).equals("") || s.substring(0, dotLoc).equals("+") || 
-                 s.substring(0, dotLoc).equals("-")) && isIntegerWithoutSymbol(s.substring(dotLoc + 1))))
-                return true;
-        }
-        else { // eCount == 1, dotCount == 1
-            if (eLoc < dotLoc) return false;
-            String s1 = s.substring(0, dotLoc);
-            String s2 = s.substring(dotLoc + 1, eLoc);
-            String s3 = s.substring(eLoc + 1);
-            if (isInteger(s3)) {
-                return isNumber(s1 + s2);
+        if (ePos == -1) return isFloating(s);
+        else return isFloating(s.substring(0, ePos)) && isInteger(s.substring(ePos + 1));
+    }
+    
+    private boolean isFloating(String s) {
+        if (s.length() == 0) return false;
+        int dotPos = -1;
+        int i = 0; // need to remove the symbol first. 
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') ++i;
+        int start = i;
+        
+        for (; i < s.length(); ++i) {
+            if (s.charAt(i) == '.') {
+                dotPos = i;
+                break;
             }
         }
-        return false;
+        if (dotPos == -1) {
+            return isUnsignedInteger(s.substring(start));
+        } else {
+            if (start == dotPos) {
+                return isUnsignedInteger(s.substring(dotPos + 1));
+            } else if (dotPos == s.length() - 1) {
+                return isUnsignedInteger(s.substring(start, dotPos));
+            } else {
+                return isUnsignedInteger(s.substring(start, dotPos)) 
+                        && isUnsignedInteger(s.substring(dotPos + 1));
+            }
+        }
+    }
+    
+    private boolean isInteger(String s) {
+        if (s.length() == 0) return false;
+        int i = 0;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') ++i;
+        return isUnsignedInteger(s.substring(i));
         
     }
-    public boolean isInteger(String s) {
+    
+    private boolean isUnsignedInteger(String s) {
         if (s.length() == 0) return false;
-        if (s.charAt(0) == '-' || s.charAt(0) == '+') return isIntegerWithoutSymbol(s.substring(1));
-        else return isIntegerWithoutSymbol(s);
-    }
-    public boolean isIntegerWithoutSymbol(String s) {
-        if (s.equals("")) return false;
         for (int i = 0; i < s.length(); ++i) {
             if (!Character.isDigit(s.charAt(i))) return false;
         }
