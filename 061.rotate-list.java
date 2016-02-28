0e1f8aa91685a90bd2e7277c22b556733a076061
douglasleer <douglasleer@gmail.com>
2016-02-28 03:18:04 -0500

    Update 061.rotate-list.java

--- 061.rotate-list.java
+++ 061.rotate-list.java
@@ -12,10 +12,10 @@
  */
 public class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null) return head;
+        if (head == null || head.next == null || k == 0) return head;
         int i = 0;
         ListNode cur = head; 
-        while (cur != null) {
+        while (cur != null) { // get the total count of the nodes
             ++i;
             cur = cur.next;
         }
@@ -23,9 +23,6 @@ public class Solution {
         k = k % n;
         if (k == 0) return head;
         
-        
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
         cur = head;
         for (int j = 1; j < n - k; ++j) {
             cur = cur.next;
@@ -35,9 +32,8 @@ public class Solution {
         ListNode firstEnd = firstStart;
         while (firstEnd.next != null) firstEnd = firstEnd.next;
         
-        dummy.next = firstStart;
         firstEnd.next = head;
         secondEnd.next = null;
-        return dummy.next;
+        return firstStart;
     }
 }
