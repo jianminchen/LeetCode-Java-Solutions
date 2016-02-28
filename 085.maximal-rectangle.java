ac868723818f8f5c8ad9b182434a243092354cb5
douglasleer <douglasleer@gmail.com>
2016-02-28 02:31:23 -0500

    Update 085.maximal-rectangle.java

--- 085.maximal-rectangle.java
+++ 085.maximal-rectangle.java
@@ -4,7 +4,8 @@
 
 public class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        if (matrix == null) throw new NullPointerException();
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
         int[] height = new int[matrix[0].length];
         for (int j = 0; j < matrix[0].length; ++j) {
             int k = 0;
@@ -17,8 +18,9 @@ public class Solution {
         for (int i = 1; i < matrix.length; ++i) {
             int[] nextHeight = new int[matrix[0].length];
             for (int j = 0; j < matrix[0].length; ++j) {
-                if (height[j] >= 2) nextHeight[j] = height[j] - 1;
-                else {
+                if (height[j] >= 2) {
+                    nextHeight[j] = height[j] - 1;
+                } else {
                     int k = i;
                     while (k < matrix.length && matrix[k][j] == '1') {
                         ++nextHeight[j];
@@ -32,40 +34,19 @@ public class Solution {
         }
         return maxArea;
     }
-    public int largestRectangleArea(int[] height) {
-        List<Integer> hList = new ArrayList<>();
-        hList.add(0);
-        for (int i = 0; i < height.length; ++i) {
-            hList.add(height[i]);
-        }
-        hList.add(0);
-        Stack<Integer> stkH = new Stack<>();
-        Stack<Integer> stkI = new Stack<>();
-        stkH.push(0);
-        stkI.push(0);
+    
+    private int largestRectangleArea(int[] height) {
+        Stack<Integer> stk = new Stack<>();
+        stk.push(-1); // stack of indexes. push -1 for consistency.
         int maxArea = 0;
-        for (int i = 1; i < hList.size(); ++i) {
-            if (hList.get(i) >= stkH.peek()) {
-                stkH.push(hList.get(i));
-                stkI.push(i);
+        for (int i = 0; i < height.length; ++i) {
+            while (stk.peek() != -1 && height[stk.peek()] > height[i]) {
+                maxArea = Math.max(maxArea, height[stk.pop()] * (i - (stk.peek() + 1)));
             }
-            // if skipping hList.get(i) == stkH.peek(), wrong answer for input:
-            // [0, 9]; output 18, expected 9 !!!!!!!!!!
-            // else if (hList.get(i) == stkH.peek()) ;
-            else { // hList.get(i) < stkH.peek();
-                while (hList.get(i) < stkH.peek()) {
-                    int area = 0;
-                    int high = stkH.pop();
-                    // wrong !! for input: 2 1 2
-                    // int index = stkI.pop();
-                    stkI.pop();
-                    int index = stkI.peek() + 1;
-                    area = high * (i - index);
-                    if (area > maxArea) maxArea = area;
-                }
-                stkH.push(hList.get(i));
-                stkI.push(i);
-            }
+            stk.push(i);
+        }
+        while (stk.peek() != -1 && height[stk.peek()] > 0) {
+            maxArea = Math.max(maxArea, height[stk.pop()] * (height.length - (stk.peek() + 1)));
         }
         return maxArea;
     }
