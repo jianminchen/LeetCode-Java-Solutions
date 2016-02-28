2beb58fe577f13c4031fb23b8c529fcb134351cf
douglasleer <douglasleer@gmail.com>
2016-02-28 16:49:01 -0500

    Update 161.one-edit-distance.java

--- 161.one-edit-distance.java
+++ 161.one-edit-distance.java
@@ -4,18 +4,18 @@
 
 public class Solution {
     public boolean isOneEditDistance(String s, String t) {
+        if (s == null || t == null) throw new NullPointerException();
         if (Math.abs(s.length() - t.length()) >= 2) return false;
         if (s.length() == t.length()) {
             int i = 0;
             while (i < s.length() && s.charAt(i) == t.charAt(i)) ++i;
-            if (i == s.length()) return false;
-            else {
+            if (i == s.length()) {
+                return false;
+            } else {
                 return s.substring(i + 1).equals(t.substring(i + 1));
             }
-        }
-        else {
+        } else {
             if (s.length() < t.length()) return isOneEditDistance(t, s);
-            
             int i = 0;
             while (i < s.length() && i < t.length() && s.charAt(i) == t.charAt(i)) ++i;
             return s.substring(i + 1).equals(t.substring(i));
