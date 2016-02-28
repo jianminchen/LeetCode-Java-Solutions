299d6bcf0dd4ff1d4e7d70ea97742856c5f0eeb8
douglasleer <douglasleer@gmail.com>
2016-02-28 04:49:07 -0500

    Update 121.best-time-to-buy-and-sell-stock.java

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -4,15 +4,14 @@
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0 || prices.length == 1) return 0;
+        if (prices == null) throw new NullPointerException();
+        if (prices.length == 0 || prices.length == 1) return 0;
         int low = prices[0];
         int maxProfit = 0;
         for (int i = 0; i < prices.length; ++i) {
             if (prices[i] >= low) {
                 maxProfit = Math.max(maxProfit, prices[i] - low);
-            }
-            else // prices[i] < low
-            {
+            } else { // prices[i] < low
                 low = prices[i];
             }
         }
