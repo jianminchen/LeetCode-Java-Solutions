f76daffe68b2486760fa47cf8cd7909388b91366
douglasleer <douglasleer@gmail.com>
2016-02-28 05:55:10 -0500

    Update 125.valid-palindrome.java

--- 125.valid-palindrome.java
+++ 125.valid-palindrome.java
@@ -4,26 +4,27 @@
 
 public class Solution {
     public boolean isPalindrome(String s) {
-        if (s == null || s.equals("")) return true;
+        if (s == null) throw new NullPointerException();
         int i = 0, j = s.length() - 1;
         while (i < j) {
-            if (!Character.isAlphabetic(s.charAt(i)) && !Character.isDigit(s.charAt(i)) ) i ++;
-            else if  (!Character.isAlphabetic(s.charAt(j)) && !Character.isDigit(s.charAt(j))) j --;
-            else {
+            if (!Character.isAlphabetic(s.charAt(i)) && !Character.isDigit(s.charAt(i))) {
+                ++i;
+            } else if  (!Character.isAlphabetic(s.charAt(j)) && !Character.isDigit(s.charAt(j))) {
+                --j;
+            } else {
                 if (Character.isDigit(s.charAt(i)) || Character.isDigit(s.charAt(j))) {
                     // at least one is digit.
-                    if (s.charAt(i) != s.charAt(j)) return false;
-                    i ++; j --;
-                }
-                // both are alphabetic
-                else {
-                    if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                    if (s.charAt(i) != s.charAt(j)) {
                         return false;
-                    i ++; j --;
+                    }
+                } else { // both are alphabetic
+                    if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                        return false;
+                    }
                 }
+                ++i;
+                --j;
             }
-            
-            
         }
         return true;
     }
