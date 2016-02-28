a26034d50ed035f08ba7ebe78ce57a5008b10c0c
douglasleer <douglasleer@gmail.com>
2016-02-28 18:05:15 -0500

    Update 153.find-minimum-in-rotated-sorted-array.java

--- 153.find-minimum-in-rotated-sorted-array.java
+++ 153.find-minimum-in-rotated-sorted-array.java
@@ -4,18 +4,16 @@
 
 public class Solution {
     public int findMin(int[] nums) {
-        // the array is cut at some location.
-        // after that, the second part is moved to the front, and the first part is moved to the end.
         if (nums == null || nums.length == 0) return Integer.MIN_VALUE;
-        return findMin(nums, 0, nums.length - 1);
+        // an iterative binary search is better, be aware of the stack memory usage for recursive methods.
+        return findMin(nums, 0, nums.length - 1); 
     }
     private int findMin(int[] nums, int low, int high) {
         if (low == high) return nums[low];
-        int mid = low + ((high - low)>>1);
+        int mid = low + ((high - low) >> 1);
         if (nums[mid] > nums[high]) {
             return findMin(nums, mid + 1, high);
-        }
-        else if (nums[mid] < nums[high]) {
+        } else if (nums[mid] < nums[high]) {
             return findMin(nums, low, mid);
         }
         return Integer.MIN_VALUE;
