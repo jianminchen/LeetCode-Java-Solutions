8b5d978b250f388b569703491a78dbdbf4294d73
douglasleer <douglasleer@gmail.com>
2016-02-28 04:04:45 -0500

    Update 110.balanced-binary-tree.java

--- 110.balanced-binary-tree.java
+++ 110.balanced-binary-tree.java
@@ -11,47 +11,38 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-    HashMap<TreeNode, Integer> hm;
     public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        hm = new HashMap<TreeNode, Integer>();
-        calculateHeight(root); // do a postOrder traversal to calculate the height of all nodes.
-        return myIsBalanced(root);
-    }
-    public boolean myIsBalanced(TreeNode root) {
-        if (root == null) return true;
+        Map<TreeNode, Integer> heightMap = new HashMap<>();
+        heightMap.put(null, 0);
         
-        if (!myIsBalanced(root.left)) return false;
-        if (!myIsBalanced(root.right)) return false;
-        if (Math.abs(hm.get(root.left) - hm.get(root.right)) > 1) return false;
-        return true;
-    }
-    public void calculateHeight(TreeNode root) {
-        // hashmap can put null as key; we can make use of this
-        hm = new HashMap<TreeNode, Integer>();
-        hm.put(null, 0);
+        Stack<TreeNode> stk = new Stack<>();
         TreeNode cur = root;
-        Stack<TreeNode> stk = new Stack<TreeNode>();
-        while (true) {
+        int left = 0, right = 0;
+        while (true) { // determine during postorder traversal
             while (cur != null) {
                 stk.push(cur);
                 cur = cur.left;
             }
+            while (!stk.isEmpty() && (stk.peek().right == null || stk.peek().right == cur)) {
+                cur = stk.pop();
+                left = heightMap.get(cur.left);
+                right = heightMap.get(cur.right);
+                if (Math.abs(left - right) >= 2) return false;
+                heightMap.put(cur, 1 + Math.max(left, right));
+            }
             if (stk.isEmpty()) break;
-            TreeNode top = stk.peek();
-            if (top.right != null) {
-                cur = top.right;
-            }
-            else {
-                do {
-                    cur = stk.pop();
-                    hm.put(cur, 1 + Math.max(hm.get(cur.left), hm.get(cur.right)));
-                } while (!stk.isEmpty() && (stk.peek().right == null || cur == stk.peek().right));
-                
-                if (stk.isEmpty()) break;
-                else cur = stk.peek().right;
-            }
+            else cur = stk.peek().right;
         }
+        return true;
     }
 }
