311f29d632431444d5dc51e5103246bd30f5a6b1
douglasleer <douglasleer@gmail.com>
2016-02-28 18:44:39 -0500

    Update 233.number-of-digit-one.java

--- 233.number-of-digit-one.java
+++ 233.number-of-digit-one.java
@@ -6,25 +6,20 @@ public class Solution {
     public int countDigitOne(int n) {
         if (n <= 0) return 0;
         else if (n <= 9) return 1;
-        
         int num = n;
         int complete = 1;
-        
-        
         num = num / 10;
         while (num != 0) {
             num = num / 10;
             complete = complete * 10;
-        }       
-        
+        }
         // complete is the complete 10, 100, 1000, numbers for n.
 
         int highest = n / complete;
         int count = 0; 
         if (highest == 1) {
             count += (n % complete + 1) + countDigitOne(complete - 1) + countDigitOne(n % complete);
-        }
-        else { // highest >= 2.
+        } else { // highest >= 2.
             count += complete;
             count += highest * countDigitOne(complete - 1) + countDigitOne(n % complete);
         }
