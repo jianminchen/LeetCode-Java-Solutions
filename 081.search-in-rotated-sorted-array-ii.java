f0ca464f860d0c37f4c20f10ebc4d5669d3d7b90
douglasleer <douglasleer@gmail.com>
2016-02-28 02:20:24 -0500

    Update 081.search-in-rotated-sorted-array-ii.java

--- 081.search-in-rotated-sorted-array-ii.java
+++ 081.search-in-rotated-sorted-array-ii.java
@@ -4,35 +4,17 @@
 
 public class Solution {
     public boolean search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
+        int low = 0, high = nums.length - 1;
         while (true) {
             if (low > high) return false;
-            int mid = low + ((high - low)>>1);
-            if (target == nums[low]) return true;
-            if (target == nums[high]) return true;
-            if (target == nums[mid]) return true;
-            
-            if (nums[mid] > nums[high]) {
-                if (target > nums[mid]) {
-                    low = mid + 1;
-                }
-                else {
-                    // target < nums[mid]
-                    ++low;
-                    --high;
-                }
-            }
-            else if (nums[mid] < nums[low]) {
-                if (target < nums[mid]) {
-                    high = mid - 1;
-                }
-                else { // target > nums[mid]
-                    --high;
-                    ++low;
-                }
-            }
-            else {
+            int mid = low + ((high - low) >> 1);
+            if (target == nums[low] || target == nums[mid] || target == nums[high]) return true;
+
+            if (nums[mid] > nums[high] && target > nums[mid]) {
+                low = mid + 1;
+            } else if (nums[mid] < nums[low] && target < nums[mid]) {
+                high = mid - 1;
+            } else {
                 ++low;
                 --high;
             }
