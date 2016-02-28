059a7ad0400df30dcb5ddb416ccf6e5b0c82ebe1
douglasleer <douglasleer@gmail.com>
2016-02-28 17:53:09 -0500

    Update 189.rotate-array.java

--- 189.rotate-array.java
+++ 189.rotate-array.java
@@ -1,22 +1,23 @@
 /**
  * @see <a href="https://leetcode.com/problems/rotate-array/">Rotate Array</a>
  */
-
+ 
 public class Solution {
     public void rotate(int[] nums, int k) {
-
+        if (nums == null) throw new NullPointerException();
+        if (k < 0) throw new IllegalArgumentException();
         k = k % nums.length;
-        int secondHalf[] = new int[k];
-        int n = nums.length;
-        for (int j = 0; j < k; j ++) {
-            secondHalf[k - 1- j] = nums[n - 1 -j]; 
-        }
-        // move
-        for (int j = 0; j < n - k; j ++) {
-            nums[n - 1 -j] = nums[n - 1 -j - k];
-        }
-        for (int j = 0; j < k; j ++) {
-            nums[j] = secondHalf[j];
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+    
+    private void reverse(int[] nums, int si, int ei) {
+        if (si >= ei) return;
+        for (int i = 0; i < (ei - si + 1) >> 1; ++i) {
+            nums[si + i] = nums[si + i] ^ nums[ei - i];
+            nums[ei - i] = nums[si + i] ^ nums[ei - i];
+            nums[si + i] = nums[si + i] ^ nums[ei - i];
         }
     }
 }
