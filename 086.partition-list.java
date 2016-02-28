4b8cb54808633fb426248facd498111b33145649
douglasleer <douglasleer@gmail.com>
2016-02-28 02:35:32 -0500

    Update 086.partition-list.java

--- 086.partition-list.java
+++ 086.partition-list.java
@@ -1,7 +1,7 @@
 /**
  * @see <a href="https://leetcode.com/problems/partition-list/">Partition List</a>
  */
-
+ 
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -12,30 +12,22 @@
  */
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode smallEnd = dummy;
-        while (smallEnd.next != null && smallEnd.next.val < x) smallEnd = smallEnd.next;
-        ListNode greatStart = smallEnd.next;
-        if (greatStart == null) return dummy.next;
-        ListNode greatEnd = greatStart;
-        while (greatEnd.next != null && greatEnd.next.val >= x) greatEnd = greatEnd.next;
-        
-        ListNode cur = greatEnd.next;
+        ListNode dummy1 = new ListNode(0);
+        ListNode dummy2 = new ListNode(0);
+        ListNode prev1 = dummy1, prev2 = dummy2;
+        ListNode cur = head;
         while (cur != null) {
-            ListNode nextSmallStart = cur;
-            while (cur.next != null && cur.next.val < x) cur = cur.next;
-            ListNode nextSmallEnd = cur;
-            ListNode nextGreatStart = cur.next;
-            smallEnd.next = nextSmallStart;
-            nextSmallEnd.next = greatStart;
-            greatEnd.next = nextGreatStart;
-            
-            // update smallEnd, greatEnd.
-            smallEnd = nextSmallEnd;
-            while (greatEnd.next != null && greatEnd.next.val >= x) greatEnd = greatEnd.next;
-            cur = greatEnd.next;
+            if (cur.val < x) {
+                prev1.next = cur;
+                prev1 = prev1.next;
+            } else {
+                prev2.next = cur;
+                prev2 = prev2.next;
+            }
+            cur = cur.next;
         }
-        return dummy.next;
+        prev2.next = null;
+        prev1.next = dummy2.next;
+        return dummy1.next;
     }
 }
