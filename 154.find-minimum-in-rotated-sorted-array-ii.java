16654293d825b6ad603146535f87456b8422f12d
douglasleer <douglasleer@gmail.com>
2016-02-28 18:06:43 -0500

    Update 154.find-minimum-in-rotated-sorted-array-ii.java

--- 154.find-minimum-in-rotated-sorted-array-ii.java
+++ 154.find-minimum-in-rotated-sorted-array-ii.java
@@ -9,11 +9,12 @@ public class Solution {
         while (low <= high) {
             if (low == high) return nums[low];
             int mid = low + ((high - low)>>1);
-            if (nums[mid] > nums[high]) low = mid + 1;
-            // using high = mid - 1 is wrong !! mid can be the min.
-            else if (nums[mid] < nums[high]) high = mid;
-            else { // nums[mid] = nums[high]
-                high = high - 1;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else if (nums[mid] < nums[high]) {
+                high = mid;
+            } else { // nums[mid] = nums[high]
+                high = high - 1; // sometimes, we do not have enough conditions for binary search.
             }
         }
         return -1;
