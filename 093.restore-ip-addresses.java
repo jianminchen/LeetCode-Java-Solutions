40818c172435b5caca43dd003474fddd57d559c9
douglasleer <douglasleer@gmail.com>
2016-02-27 23:03:30 -0500

    Update 093.restore-ip-addresses.java

--- 093.restore-ip-addresses.java
+++ 093.restore-ip-addresses.java
@@ -5,55 +5,36 @@
 public class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
-        List<List<String>> intermediate = restoreIpAddresses(s, 4);
-        for (List<String> aList : intermediate) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(aList.get(0));
-            for (int i = 1; i < aList.size(); ++i) {
-                sb.append('.');
-                sb.append(aList.get(i));
-            }
-            res.add(new String(sb));
-        }
+        helper(s, 4, new ArrayList<String>(), res);
         return res;
     }
     
-    public List<List<String>> restoreIpAddresses(String s, int n) {
-        List<List<String>> res = new ArrayList<>();
-        if (s.length() < n || s.length() > 3 * n) return res;
-        if (n == 1) {
-            if (s.length() > 1 && s.charAt(0) == '0') return res;
-            if (Integer.parseInt(s) > 255) return res;
-            else {
-                List<String> aList = new ArrayList<>();
-                aList.add(s);
-                res.add(aList);
-                return res;
-            }
-        }
-        else {
-            if (s.charAt(0) == '0') {
-                for (List<String> aList : restoreIpAddresses(s.substring(1), n - 1)) {
-                    List<String> newList = new ArrayList<>();
-                    newList.add("0");
-                    newList.addAll(aList);
-                    res.add(newList);
+    private void helper(String s, int remain, List<String> path, List<String> res) {
+        if (s.length() > remain * 3 || s.length() < remain) return;
+        if (remain == 0) {
+            if (s.length() == 0) {
+                StringBuilder aRes = new StringBuilder(path.get(0));
+                for (int i = 1; i < path.size(); ++i) {
+                    aRes.append("."); 
+                    aRes.append(path.get(i));
                 }
+                res.add(new String(aRes));
             }
-            else {
-                for (int i = 1; i <= 3 && i <= s.length(); ++i) {
-                    if (Integer.parseInt(s.substring(0, i)) <= 255) {
-                        for (List<String> aList : restoreIpAddresses(s.substring(i), n - 1)) {
-                            List<String> newList = new ArrayList<>();
-                            newList.add(s.substring(0, i));
-                            newList.addAll(aList);
-                            res.add(newList);
-                        }
+        } else { // remain != 0
+            if (s.charAt(0) == '0') {
+                path.add("0");
+                helper(s.substring(1), remain - 1, path, res);
+                path.remove(path.size() - 1);
+            } else {
+                for (int i = 1; i <= Math.min(3, s.length()); ++i) {
+                    // do not need to check for two digit numbers.
+                    if (i <= 2 || Integer.parseInt(s.substring(0, i)) <= 255) {
+                        path.add(s.substring(0, i));
+                        helper(s.substring(i), remain - 1, path, res);
+                        path.remove(path.size() - 1);
                     }
                 }
-
             }
-            return res;
         }
     }
 }
