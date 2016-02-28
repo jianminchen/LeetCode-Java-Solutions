63e626715c7b7e4cf0597ebb16b73c6c13855521
douglasleer <douglasleer@gmail.com>
2016-02-28 04:50:08 -0500

    Update 122.best-time-to-buy-and-sell-stock-ii.java

--- 122.best-time-to-buy-and-sell-stock-ii.java
+++ 122.best-time-to-buy-and-sell-stock-ii.java
@@ -4,7 +4,7 @@
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        // if (prices == null) return 0;
+        if (prices == null) throw new NullPointerException();
         int max = 0;
         for (int i = 0; i + 1 < prices.length; ++i) {
             if (prices[i + 1] > prices[i]) {
