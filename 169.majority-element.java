192c08768a76405e8459e29cba1208e0ec315349
douglasleer <douglasleer@gmail.com>
2016-02-28 17:16:37 -0500

    Update 169.majority-element.java

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -4,15 +4,15 @@
 
 public class Solution {
     public int majorityElement(int[] nums) {
-        // nums is assumed to be non-empty
+        if (nums == null) throw new NullPointerException();
+        if (nums.length == 0) throw new IllegalArgumentException();
         int cand = nums[0];
         int count = 1;
-        for (int i = 1; i < nums.length; i ++) {
+        for (int i = 1; i < nums.length; ++i) {
             if (cand == nums[i]) {
-                count ++;
-            }
-            else { // cand != nums[i]
-                count --;
+                ++count;
+            } else { // cand != nums[i]
+                --count;
                 if (count == 0) {
                     cand = nums[i];
                     count = 1;
