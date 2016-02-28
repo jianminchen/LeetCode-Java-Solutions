100177e2919f227df29e67ddcf28d42f457abd71
douglasleer <douglasleer@gmail.com>
2016-02-28 17:56:33 -0500

    Update 191.number-of-1-bits.java

--- 191.number-of-1-bits.java
+++ 191.number-of-1-bits.java
@@ -1,17 +1,15 @@
 /**
  * @see <a href="https://leetcode.com/problems/number-of-1-bits/">Number of 1 Bits</a>
  */
-
+ 
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        int w = 0;
-        int i = 0;
-        while (i < 32) {
-            w += (n&1);
-            n = n>>1;
-            i ++;
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1); // clear the last 1 bit
+            ++count;
         }
-        return w;
+        return count;
     }
 }
