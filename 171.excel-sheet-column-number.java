96e1b2295e05b1e5728ccf3e16abf2c837956dc1
douglasleer <douglasleer@gmail.com>
2016-02-28 17:18:28 -0500

    Update 171.excel-sheet-column-number.java

--- 171.excel-sheet-column-number.java
+++ 171.excel-sheet-column-number.java
@@ -4,10 +4,10 @@
 
 public class Solution {
     public int titleToNumber(String s) {
-        if (s == null || s.length() == 0) return 0;
-        int multiplier = 1;
-        int res = 0;
-        for (int i = s.length() - 1; i >= 0; i --) {
+        if (s == null) throw new NullPointerException();
+        if (s.length() == 0) return 0;
+        int multiplier = 1, res = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
             int num = s.charAt(i)  - 'A' + 1;
             res += num * multiplier;
             multiplier = multiplier * 26;
