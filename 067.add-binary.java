5a13bf6d0218930c4a68f8143c3282e09a8afc41
douglasleer <douglasleer@gmail.com>
2016-02-27 23:35:48 -0500

    Update 067.add-binary.java

--- 067.add-binary.java
+++ 067.add-binary.java
@@ -4,14 +4,11 @@
 
 public class Solution {
     public String addBinary(String a, String b) {
-        if (a == null) return b;
-        if (b == null) return a;
-        // if s.length() < b.length();
+        if (a == null || b == null) throw new NullPointerException();
         if (a.length() < b.length()) return addBinary(b, a);
         // now we can assume that a.length() >= b.length()
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+        Stack<Character> stk = new Stack<>();
+        int i = a.length() - 1, j = b.length() - 1;
         int carryIn = 0;
         for (; i >= 0; i --, j --) {
             int iA = a.charAt(i) - '0';
@@ -22,9 +19,11 @@ public class Solution {
             int sum = iA + iB + carryIn;
             carryIn = sum / 2;
             int num = sum % 2;
-            sb.insert(0, (char) (num + '0'));
+            stk.push((char) (num + '0'));
         }
-        if (carryIn != 0) sb.insert(0, (char) '1');
+        if (carryIn != 0) stk.push((char) '1');
+        StringBuilder sb = new StringBuilder();
+        while (!stk.isEmpty()) sb.append(stk.pop());
         return new String(sb);
     }
 }
