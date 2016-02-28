d29283ce1ec5c844d9389d452fac1bfff9744274
douglasleer <douglasleer@gmail.com>
2016-02-28 06:31:58 -0500

    Update 304.range-sum-query-2d-immutable.java

--- 304.range-sum-query-2d-immutable.java
+++ 304.range-sum-query-2d-immutable.java
@@ -5,8 +5,8 @@
 public class NumMatrix {
     int[][] sumMatrix;
     public NumMatrix(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || 
-            matrix[0] == null || matrix[0].length == 0) return;
+        if (matrix == null || matrix.length == 0
+                || matrix[0] == null || matrix[0].length == 0) return;
         int m = matrix.length;
         int n = matrix[0].length;
         sumMatrix = new int[m + 1][n + 1];
@@ -26,8 +26,8 @@ public class NumMatrix {
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
         if (sumMatrix == null) return 0;
-        return sumMatrix[row2 + 1][col2 + 1] - sumMatrix[row1][col2 + 1] - 
-               sumMatrix[row2 + 1][col1] + sumMatrix[row1][col1];
+        return sumMatrix[row2 + 1][col2 + 1] - sumMatrix[row1][col2 + 1] 
+                - sumMatrix[row2 + 1][col1] + sumMatrix[row1][col1];
     }
 }
 
