3afb31b0af46d554b69a58360a80ef1232bdae0b
douglasleer <douglasleer@gmail.com>
2016-02-28 17:21:48 -0500

    Update 187.repeated-dna-sequences.java

--- 187.repeated-dna-sequences.java
+++ 187.repeated-dna-sequences.java
@@ -4,29 +4,25 @@
 
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-
         List<String> res = new ArrayList<>();
         if (s == null || s.length() <= 10) return res;
         Map<Integer, Boolean> hm = new HashMap<>();
-
         for (int i = 0; i <= s.length() - 10; ++i) {
             String sub = s.substring(i, i + 10);
             int num = toNumber(sub);
-  
             if (hm.containsKey(num)) {
                 if (hm.get(num) == false) {
                     res.add(sub);
                     hm.put(num, true);
                 }
-            }
-            else {
+            } else {
                 hm.put(num, false); // appear before but not added to res yet.
             }
         }
         return res;
     }
     
-    public int toNumber(String s) {
+    private int toNumber(String s) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); ++i) {
             char c = s.charAt(i);
