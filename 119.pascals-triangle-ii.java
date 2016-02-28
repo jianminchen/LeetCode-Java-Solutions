78aa3e29eaad0d4313345d4a24cb006b1ef0e139
douglasleer <douglasleer@gmail.com>
2016-02-28 04:48:18 -0500

    Update 119.pascals-triangle-ii.java

--- 119.pascals-triangle-ii.java
+++ 119.pascals-triangle-ii.java
@@ -4,32 +4,18 @@
 
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
-        // rowIndex starts from 0.
-        List<Integer> li = new ArrayList<Integer>();
-        if (rowIndex < 0) return li;
-        if (0 == rowIndex) {
-            li.add(1);
-            return li;
-        }
-        if (1 == rowIndex) {
-            li.add(1);
-            li.add(1);
-            return li;
-        }
-        List<Integer> newList = new ArrayList<Integer>();
-        newList.add(1); newList.add(1);
-        for (int k = 2; k <= rowIndex; k ++) {
-            int prev = 1;
-            for (int j = 1; j < k; j ++) {
-                // use the prev value to calculate
-                int newValue = prev + newList.get(j);
-                // update the prev before newList.get(j) is overwritten.
-                prev = newList.get(j);
-                // update the newList.get(j), when prev has stored its prev value.
-                newList.set(j, newValue);
+        if (rowIndex < 0) throw new IllegalArgumentException();
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        for (int i = 1; i <= rowIndex; ++i) {
+            List<Integer> nextList = new ArrayList<Integer>();
+            nextList.add(1);
+            for (int j = 1; j < i; ++j) {
+                nextList.add(res.get(j - 1) + res.get(j));
             }
-            newList.add(1);
+            nextList.add(1);
+            res = nextList;
         }
-        return newList;
+        return res;
     }
 }
