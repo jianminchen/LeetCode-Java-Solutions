00915292d571443f6ea04859d7779d4d10c03175
douglasleer <douglasleer@gmail.com>
2016-02-28 18:23:57 -0500

    Update 226.invert-binary-tree.java

--- 226.invert-binary-tree.java
+++ 226.invert-binary-tree.java
@@ -14,10 +14,8 @@
 public class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-        root.left = right;
-        root.right = left;
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
         return root;
     }
 }
