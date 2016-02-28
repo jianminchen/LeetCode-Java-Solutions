1574296fad84458ad90a6ba72c0681605986dfdc
douglasleer <douglasleer@gmail.com>
2016-02-28 03:38:10 -0500

    Update 103.binary-tree-zigzag-level-order-traversal.java

--- 103.binary-tree-zigzag-level-order-traversal.java
+++ 103.binary-tree-zigzag-level-order-traversal.java
@@ -19,17 +19,17 @@ public class Solution {
         List<Integer> firstList = new ArrayList<>();
         res.add(firstList);
         q.add(root);
-        q.add(null);
+        q.add(null); // to indicate the end of a level
         while (!q.isEmpty()) {
             TreeNode n = q.remove();
-            if (q.isEmpty()) break;
-            else {
+            if (q.isEmpty()) {
+                break;
+            } else {
                 if (n == null) {
                     List<Integer> newList = new ArrayList<>();
                     res.add(newList);
                     q.add(null);
-                }
-                else { // n is a node
+                } else { // n is a node
                     res.get(res.size() - 1).add(n.val); // add here
                     if (n.left != null) q.add(n.left);
                     if (n.right != null) q.add(n.right);
