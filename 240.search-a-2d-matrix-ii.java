038a0880f5154f5414a0173c0bc69bf33d7d8c62
douglasleer <douglasleer@gmail.com>
2016-02-28 19:07:51 -0500

    Update 240.search-a-2d-matrix-ii.java

--- 240.search-a-2d-matrix-ii.java
+++ 240.search-a-2d-matrix-ii.java
@@ -10,16 +10,16 @@ public class Solution {
     public boolean searchMatrix(int[][] matrix, int target, int ia, int ja, int ib, int jb) {
         if (ia > ib || ja > jb) return false;
         if (target < matrix[ia][ja] || target > matrix[ib][jb]) return false;
-        int imid = ia + ((ib - ia)>>1);
-        int jmid = ja + ((jb - ja)>>1);
-        if (target == matrix[imid][jmid]) return true;
-        else if (target < matrix[imid][jmid]) {
-            return searchMatrix(matrix, target, ia, ja, imid - 1, jb) ||
-                   searchMatrix(matrix, target, imid, ja, ib, jmid - 1);
-        }
-        else { // target > matrix[imid][jmid]
-            return searchMatrix(matrix, target, imid + 1, ja, ib, jmid) || 
-                   searchMatrix(matrix, target, ia, jmid + 1, ib, jb);
+        int imid = ia + ((ib - ia) >> 1);
+        int jmid = ja + ((jb - ja) >> 1);
+        if (target == matrix[imid][jmid]) {
+            return true;
+        } else if (target < matrix[imid][jmid]) {
+            return searchMatrix(matrix, target, ia, ja, imid - 1, jb) 
+                    || searchMatrix(matrix, target, imid, ja, ib, jmid - 1);
+        } else { // target > matrix[imid][jmid]
+            return searchMatrix(matrix, target, imid + 1, ja, ib, jmid)
+                    || searchMatrix(matrix, target, ia, jmid + 1, ib, jb);
         }
     }
 }
