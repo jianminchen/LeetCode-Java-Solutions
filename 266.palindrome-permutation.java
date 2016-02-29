830ac912996631731bf4b186721c3e1c5e37d09f
douglasleer <douglasleer@gmail.com>
2016-02-29 05:50:06 -0500

    Update 266.palindrome-permutation.java

--- 266.palindrome-permutation.java
+++ 266.palindrome-permutation.java
@@ -6,14 +6,13 @@ public class Solution {
     public boolean canPermutePalindrome(String s) {
         if (s == null) return true;
         HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
-        for (int i = 0; i < s.length(); i ++) {
+        for (int i = 0; i < s.length(); ++i) {
             hm.put(s.charAt(i), hm.containsKey(s.charAt(i)) ? hm.get(s.charAt(i)) + 1 : 1);
         }
         int oddCount = 0;
-        Set<Character> sc = hm.keySet();
-        for(Character c : sc) {
+        for(Character c : hm.keySet()) {
             if (hm.get(c) % 2 != 0) {
-                oddCount ++;
+                ++oddCount;
                 if (oddCount > 1) return false;
             }
         }
