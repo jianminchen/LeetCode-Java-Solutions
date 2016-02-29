2ab2ce91d3074a9dcce737c77c5d1de2d3eff64c
douglasleer <douglasleer@gmail.com>
2016-02-29 05:59:13 -0500

    Update 290.word-pattern.java

--- 290.word-pattern.java
+++ 290.word-pattern.java
@@ -10,21 +10,20 @@ public class Solution {
         HashSet<String> valueSet = new HashSet<String>();
         
         int si = 0; // where we are in the string.
-        for (int i = 0; i < pattern.length(); i ++) {
+        for (int i = 0; i < pattern.length(); ++i) {
             if (si >= str.length()) return false;
             int start = si;
-            while (si < str.length() && str.charAt(si) != ' ') si ++;
+            while (si < str.length() && str.charAt(si) != ' ') ++si;
             int end = si;
             String word = str.substring(start, end);
             if (hm.containsKey(pattern.charAt(i))) {
                 if (!word.equals(hm.get(pattern.charAt(i)))) return false;
-            }
-            else {
+            } else {
                 hm.put(pattern.charAt(i), word);
                 if (valueSet.contains(word)) return false;
                 valueSet.add(word);
             }
-            si ++; // points to the beginning of the next word.
+            ++si; // points to the beginning of the next word.
         }
         // pattern finishes, but there are still some words in the string.
         if (si < str.length()) return false; 
