3fe6ff122d5a9921bd3f1fc3eb09118b101c08f2
douglasleer <douglasleer@gmail.com>
2016-02-28 19:05:58 -0500

    Update 236.lowest-common-ancestor-of-a-binary-tree.java

--- 236.lowest-common-ancestor-of-a-binary-tree.java
+++ 236.lowest-common-ancestor-of-a-binary-tree.java
@@ -11,16 +11,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
- 
+
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         Stack<TreeNode> stk = new Stack<>();
