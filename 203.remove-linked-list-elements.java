c75f2de0f693cdb79a8fe597b27ceb23f9c738a3
douglasleer <douglasleer@gmail.com>
2016-02-29 03:09:53 -0500

    Update 203.remove-linked-list-elements.java

--- 203.remove-linked-list-elements.java
+++ 203.remove-linked-list-elements.java
@@ -10,14 +10,6 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode(0), prev = dummy;
