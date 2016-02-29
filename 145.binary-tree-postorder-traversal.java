1337de8b8e5d8cb00018d2031832bf2ffe0e2a96
douglasleer <douglasleer@gmail.com>
2016-02-29 18:04:26 -0500

    Update 145.binary-tree-postorder-traversal.java

--- 145.binary-tree-postorder-traversal.java
+++ 145.binary-tree-postorder-traversal.java
@@ -13,25 +13,21 @@
  */
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<Integer>();
-        Stack<TreeNode> stk = new Stack<TreeNode>();
+        List<Integer> res = new ArrayList<>();
         TreeNode cur = root;
+        Stack<TreeNode> stk = new Stack<>();
         while (true) {
             while (cur != null) {
                 stk.push(cur);
                 cur = cur.left;
             }
-            if (stk.isEmpty()) break;
-            if (stk.peek().right != null ) cur = stk.peek().right;
-            else {
-                do {
-                    cur = stk.pop();
-                    list.add(cur.val);
-                } while (!stk.isEmpty() && (stk.peek().right == null || cur == stk.peek().right));
-                if (stk.isEmpty()) break;
-                else cur = stk.peek().right;
+            while (!stk.isEmpty() && (stk.peek().right == null || stk.peek().right == cur)){
+                cur = stk.pop();
+                res.add(cur.val);
             }
+            if (stk.isEmpty()) break;
+            else cur = stk.peek().right;
         }
-        return list;
+        return res;
     }
 }
