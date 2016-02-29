d126ae1a7967e44281f6036870b462184ebbb5c3
douglasleer <douglasleer@gmail.com>
2016-02-29 05:04:54 -0500

    Update 266.palindrome-permutation.java

--- 266.palindrome-permutation.java
+++ 266.palindrome-permutation.java
@@ -7,12 +7,7 @@ public class Solution {
         if (s == null) return true;
         HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
         for (int i = 0; i < s.length(); i ++) {
-            if (!hm.containsKey(s.charAt(i))) {
-                hm.put(s.charAt(i), 1);
-            }
-            else {
-                hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
-            }
+            hm.put(s.charAt(i), hm.containsKey(s.charAt(i)) ? hm.get(s.charAt(i)) + 1 : 1);
         }
         int oddCount = 0;
         Set<Character> sc = hm.keySet();
