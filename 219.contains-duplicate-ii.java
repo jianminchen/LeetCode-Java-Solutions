acf21046d94ad8974fa287a77cd0b722256ff988
douglasleer <douglasleer@gmail.com>
2016-02-29 04:29:53 -0500

    Update 219.contains-duplicate-ii.java

--- 219.contains-duplicate-ii.java
+++ 219.contains-duplicate-ii.java
@@ -4,25 +4,15 @@
 
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // a hashMap stores an array of indices of the number.
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<Integer, ArrayList<Integer>>();
+        // a hashMap stores the last index of the number.
+        Map<Integer, Integer> hm = new HashMap<>();
         if (nums == null || nums.length == 0) return false;
         if (k <= 0) return false;
-        for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(nums[i])) hm.get(nums[i]).add(i);
-            else {
-                ArrayList<Integer> al = new ArrayList<Integer>();
-                al.add(i);
-                hm.put(nums[i], al);
+        for (int i = 0; i < nums.length; ++i) {
+            if (hm.containsKey(nums[i])) {
+                if (i - hm.get(nums[i]) <= k) return true;
             }
-        }
-
-        Set<Map.Entry<Integer, ArrayList<Integer>>> set = hm.entrySet();
-        for (Map.Entry<Integer, ArrayList<Integer>> entry : set) {
-            Collections.sort(entry.getValue());
-            for (int i = 1; i < entry.getValue().size(); i ++) {
-                if (entry.getValue().get(i) - entry.getValue().get(i-1) <= k) return true;
-            }
+            hm.put(nums[i], i); // add new or update
         }
         return false;
     }
