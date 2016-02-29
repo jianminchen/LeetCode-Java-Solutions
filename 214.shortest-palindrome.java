b6671335f4669f5040de2cb47d37363a08fd14ab
douglasleer <douglasleer@gmail.com>
2016-02-29 04:11:22 -0500

    Update 214.shortest-palindrome.java

--- 214.shortest-palindrome.java
+++ 214.shortest-palindrome.java
@@ -10,11 +10,11 @@ public class Solution {
                 fail[j] = k + 1;
                 ++j;
                 ++k;
-            }
-            else if (k > 0) {
+            } else if (k > 0) {
                 k = fail[k - 1];
+            } else {
+                ++j;
             }
-            else ++j;
         }
         return new StringBuilder(s.substring(fail[ns.length() - 1])).reverse().toString() + s;
     }
