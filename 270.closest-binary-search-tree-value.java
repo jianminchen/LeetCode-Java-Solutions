409c9fae1490edc90b2f064b21217d7625c81c66
douglasleer <douglasleer@gmail.com>
2016-02-29 05:27:41 -0500

    Update 270.closest-binary-search-tree-value.java

--- 270.closest-binary-search-tree-value.java
+++ 270.closest-binary-search-tree-value.java
@@ -13,53 +13,19 @@
  */
 public class Solution {
     public int closestValue(TreeNode root, double target) {
-        // method 1 : recursive solution:
-        // method 2: inorder traversal, then, binary search to find the closest value.
-        if (root.left == null && root.right == null) return root.val;
-        else if (root.left == null && root.right != null || 
-                 root.right != null && target >= root.val) {
-            if (target <= root.val) return root.val;
-            else {
-                int smallest = getSmallest(root.right);
-                if (target - root.val < smallest - target) return root.val;
-                else if (target <= smallest) {
-                    return smallest;
-                }
-                else {
-                    return closestValue(root.right, target);
-                }
+        TreeNode upper = null, lower = null;
+        TreeNode cur = root;
+        // go down towards the leaf, set the upper bound and lower bound for the target when possible.
+        while (cur != null) {
+            if (cur.val < target) {
+                lower = cur;
+                cur = cur.right;
+            } else {
+                upper = cur;
+                cur = cur.left;
             }
         }
-        else // if (root.right == null && root.left != null) 
-        {
-            if (target >= root.val) return root.val;
-            else {
-                int greatest = getGreatest(root.left);
-                if (root.val - target <= target - greatest) {
-                    return root.val;
-                }
-                else if (greatest <= target) {
-                    return greatest;
-                }
-                else {
-                    return closestValue(root.left, target);
-                }
-            }
-        }
-        
-    }
-    public int getGreatest(TreeNode root) {
-        TreeNode cur = root;
-        while (cur.right != null) {
-            cur = cur.right;
-        }
-        return cur.val;
-    }
-    public int getSmallest(TreeNode root) {
-        TreeNode cur = root;
-        while (cur.left != null) {
-            cur = cur.left;
-        }
-        return cur.val;
+        if (upper == null || (lower != null && target - lower.val < upper.val - target)) return lower.val;
+        else return upper.val;
     }
 }
