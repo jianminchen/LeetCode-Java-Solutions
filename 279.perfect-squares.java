385d0537f02da76492493374b61bc7e0940c6b5d
douglasleer <douglasleer@gmail.com>
2016-02-29 04:50:24 -0500

    Update 279.perfect-squares.java

--- 279.perfect-squares.java
+++ 279.perfect-squares.java
@@ -5,13 +5,11 @@
 public class Solution {
     public int numSquares(int n) {
         int num[] = new int[n];
-        num[0] = 1;
-        for (int i = 2; i <= n; ++i) {
-            num[i - 1] = n;
+        for (int i = 1; i <= n; ++i) {
+            num[i - 1] = i;
         }
         
         for (int i = 2; i <= n; ++i) {
-            // num[i - 1] will be the min num for i.
             int j = 1;
             for (; j < i / j; ++j) {
                 num[i-1] = Math.min(num[i - j * j - 1] + 1, num[i - 1]);
