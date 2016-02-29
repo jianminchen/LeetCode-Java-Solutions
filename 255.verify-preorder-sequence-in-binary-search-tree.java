74e6282d38b7fa5412e338777beda38602071ee9
douglasleer <douglasleer@gmail.com>
2016-02-29 05:45:08 -0500

    Update 255.verify-preorder-sequence-in-binary-search-tree.java

--- 255.verify-preorder-sequence-in-binary-search-tree.java
+++ 255.verify-preorder-sequence-in-binary-search-tree.java
@@ -7,17 +7,19 @@ public class Solution {
         if (preorder == null || preorder.length == 0) return true;
         return verifyPreorder(preorder, 0, preorder.length - 1);
     }
+    
     private boolean verifyPreorder(int[] preorder, int low, int high) {
         if(low >= high) return true;
         int i = low + 1;
         while (i <= high && preorder[low] > preorder[i]) ++i;
-        if (i == high + 1) return verifyPreorder(preorder, low + 1, high);
-        else {
+        if (i == high + 1) {
+            return verifyPreorder(preorder, low + 1, high);
+        } else {
             for (int j = i + 1; j <= high; ++j) {
                 if (preorder[j] < preorder[low]) return false;
             }
-            return verifyPreorder(preorder, low + 1, i - 1) &&
-                   verifyPreorder(preorder, i, high);
+            return verifyPreorder(preorder, low + 1, i - 1) 
+                    && verifyPreorder(preorder, i, high);
         }
     }
 }
