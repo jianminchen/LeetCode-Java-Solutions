4bbe5f733e4166a5535f032f6bb2523f43192c9c
douglasleer <douglasleer@gmail.com>
2016-02-29 17:39:11 -0500

    Update 306.additive-number.java

--- 306.additive-number.java
+++ 306.additive-number.java
@@ -15,38 +15,34 @@ public class Solution {
         }
         return false;
     }
-    public boolean isAdditiveNumber(String n1, String n2, String num) {
+    
+    private boolean isAdditiveNumber(String n1, String n2, String num) {
         String addS = addTwoStrings(n1, n2);
-        System.out.println(addS);
         if ((n1.length() > 1 && n1.charAt(0) == '0') || (n2.length() > 1 && n2.charAt(0) == '0')) return false;
-        // it is num here, not nums!!!
         if (num.length() - n1.length() - n2.length() < addS.length()) return false;
 
-        if (!addS.equals(num.substring(n1.length() + n2.length(), n1.length() + n2.length() + addS.length()))) return false;
-        else {
-            if (addS.length() + n1.length() + n2.length() == num.length()) return true;
-            else {
+        if (!addS.equals(num.substring(n1.length() + n2.length(), n1.length() + n2.length() + addS.length()))) {
+            return false;
+        } else {
+            if (addS.length() + n1.length() + n2.length() == num.length()) {
+                return true;
+            } else {
                 return isAdditiveNumber(n2, addS, num.substring(n1.length()));                
             }
         }
     }
-    public String addTwoStrings(String s1, String s2) {
-        // the result is backward. when to use string, and when to use stringbuilder?
-        // StringBuilder sb = new StringBuilder();
+    
+    private String addTwoStrings(String s1, String s2) {
         String s = new String();
         int i = s1.length() - 1, j = s2.length() - 1;
         int cIn = 0;
-        while ( i >= 0 || j >= 0) {
-            int dgt1 = 0;
-            int dgt2 = 0;
-            if (i >= 0) dgt1 = s1.charAt(i) - '0';
-            if (j >= 0) dgt2 = s2.charAt(j) - '0';
+        for (; i >= 0 || j >= 0; --i, --j) {
+            int dgt1 = i >= 0 ? s1.charAt(i) - '0' : 0;
+            int dgt2 = j >= 0 ? s2.charAt(j) - '0' : 0;
             int sum = cIn + dgt1 + dgt2;
             int dgt = sum % 10;
             cIn = sum / 10;
             s = dgt + s;
-            --i;
-            --j;
         }
         if (cIn != 0) s = cIn + s;
         return s;
