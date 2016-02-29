1502820442db0f149b6143e08d1ede87879a9432
douglasleer <douglasleer@gmail.com>
2016-02-29 03:12:34 -0500

    Update 205.isomorphic-strings.java

--- 205.isomorphic-strings.java
+++ 205.isomorphic-strings.java
@@ -9,8 +9,7 @@ public class Solution {
         for (int i = 0; i < s.length(); i ++) {
             if (hm.containsKey(s.charAt(i))) {
                 if (t.charAt(i) != hm.get(s.charAt(i))) return false;
-            }
-            else {
+            } else {
                 hm.put(s.charAt(i), t.charAt(i));
             }
         }
