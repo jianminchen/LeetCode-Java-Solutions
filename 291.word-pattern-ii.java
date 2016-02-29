15c09fcab0f85958f1e6c3d823f60173a0efd78a
douglasleer <douglasleer@gmail.com>
2016-02-29 06:05:34 -0500

    Update 291.word-pattern-ii.java

--- 291.word-pattern-ii.java
+++ 291.word-pattern-ii.java
@@ -1,3 +1,7 @@
+/**
+ * @see <a href="https://leetcode.com/problems/word-pattern-ii//">Word Pattern Match II</a>
+ */
+
 public class Solution {
     public boolean wordPatternMatch(String pattern, String str) {
     	Set<String> set = new HashSet<>();
@@ -6,29 +10,27 @@ public class Solution {
         return wordPatternMatch(set, hm, pattern, str);
     }
     
-    public boolean wordPatternMatch(Set<String> set, Map<Character, String> hm, String pattern, String str) {
+    private boolean wordPatternMatch(Set<String> set, Map<Character, String> hm, String pattern, String str) {
     	if (pattern.length() == 0 && str.length() == 0) return true;
     	else if (pattern.length() == 0 && str.length() != 0) return false;
     	else if (pattern.length() != 0 && str.length() == 0) return false;
-    	else { // both not zero.
-    	    if (hm.containsKey(pattern.charAt(0))) {
-    		String w = hm.get(pattern.charAt(0));
-    		if (str.length() < w.length()) return false;
-    		else if (!str.substring(0, w.length()).equals(w)) return false;
-    		else return wordPatternMatch(set, hm, pattern.substring(1), str.substring(w.length()));
-    	    }
-    	    else {
+
+    	if (hm.containsKey(pattern.charAt(0))) {
+    	    String w = hm.get(pattern.charAt(0));
+    	    if (str.length() < w.length()) return false;
+    	    else if (!str.substring(0, w.length()).equals(w)) return false;
+    	    else return wordPatternMatch(set, hm, pattern.substring(1), str.substring(w.length()));
+    	} else {
     		for (int i = 1; i <= str.length(); ++i) {
     		    if (!set.contains(str.substring(0, i))) {
-    			set.add(str.substring(0, i));
-    			hm.put(pattern.charAt(0), str.substring(0, i));
-    			if (wordPatternMatch(set, hm, pattern.substring(1), str.substring(i))) return true;
-    			set.remove(str.substring(0, i));
-    			m.remove(pattern.charAt(0));
+    			    set.add(str.substring(0, i));
+    			    hm.put(pattern.charAt(0), str.substring(0, i));
+    			    if (wordPatternMatch(set, hm, pattern.substring(1), str.substring(i))) return true;
+    			    set.remove(str.substring(0, i));
+    			    hm.remove(pattern.charAt(0));
     		    }
     		}
     		return false;
-    	    }
     	}
     }
 }
