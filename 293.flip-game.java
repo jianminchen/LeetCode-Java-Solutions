2f3ef4f686d63af6dcb0c8d8cb0c0d88ed3dbb78
douglasleer <douglasleer@gmail.com>
2016-02-29 06:11:34 -0500

    Update 293.flip-game.java

--- 293.flip-game.java
+++ 293.flip-game.java
@@ -7,23 +7,17 @@ public class Solution {
         int iPlus = 0;
         List<String> ls = new ArrayList<String>();
         while (true) {
-            while (iPlus < s.length() && s.charAt(iPlus) != '+') iPlus ++;
+            while (iPlus < s.length() && s.charAt(iPlus) != '+') ++iPlus;
             if (s.length() == iPlus) break;
             int iMinus = iPlus;
-            
-            while (iMinus < s.length() && s.charAt(iMinus) == '+') iMinus ++;
-            
-
-            if (iMinus == iPlus + 1) {
-                ;
-            }
+            while (iMinus < s.length() && s.charAt(iMinus) == '+') ++iMinus;
+            if (iMinus == iPlus + 1) {}
             else {
-                for (int i = iPlus; i <= iMinus - 2; i++) {
+                for (int i = iPlus; i <= iMinus - 2; ++i) {
                     String str = s.substring(0, i) + "--" + s.substring(i + 2);
                     ls.add(str);
                 }
             }
-            
             iPlus = iMinus;
         }
         return ls;
