d14863d69601195f30558778875bcca3b665329b
douglasleer <douglasleer@gmail.com>
2016-03-01 06:11:19 -0500

    Update 249.group-shifted-strings.java

--- 249.group-shifted-strings.java
+++ 249.group-shifted-strings.java
@@ -6,37 +6,35 @@ public class Solution {
     public List<List<String>> groupStrings(String[] strings) {
         // use hashMap, key is the pattern that starts with character a.
         // the values will be an arrayList of words that have the same pattern.
-        List<List<String>> lls = new ArrayList<List<String>>();
-        HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
+        List<List<String>> lls = new ArrayList<>();
+        Map<String, ArrayList<String>> hm = new HashMap<>();
         for (int i = 0; i < strings.length; i ++) {
             String pattern = shiftingBack(strings[i]);
-            if (hm.containsKey(pattern)) hm.get(pattern).add(strings[i]);
-            else {
+            if (hm.containsKey(pattern)) {
+                hm.get(pattern).add(strings[i]);
+            } else {
                 ArrayList<String> aList = new ArrayList<String>();
                 aList.add(strings[i]);
                 hm.put(pattern, aList);
             }
         }
         
-        Set<String> kSet = hm.keySet();
-        for (String s : kSet) {
+        for (String s : hm.keySet()) {
             Collections.sort(hm.get(s));
             lls.add(hm.get(s));
         }
         return lls;
-        
     }
-    public String shiftingBack(String s) {
-        // a string that may or may not starts with a.
-        // shift back to a pattern.
+    
+    private String shiftingBack(String s) {
+        // a string that may or may not starts with a, shift back to a pattern.
         int diff = s.charAt(0) - 'a';
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i ++) {
             if (s.charAt(i) - 'a' >= diff) {
                 char c = (char) (s.charAt(i) - diff);
                 sb.append(c);
-            }
-            else {
+            } else {
                 char c = (char) (s.charAt(i) - diff + 26);
                 sb.append(c);
             }
