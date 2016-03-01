ce6863ee81c95c82b6f690cb60ea705723fbf637
douglasleer <douglasleer@gmail.com>
2016-03-01 12:57:19 -0500

    Update 288.unique-word-abbreviation.java

--- 288.unique-word-abbreviation.java
+++ 288.unique-word-abbreviation.java
@@ -3,15 +3,16 @@
  */
 
 public class ValidWordAbbr {
-    // if the word correspond to a key, and the word is the only key in the words that have the same key.
-    // return true;
-    // if the key of the word does not belong to the keys of the dictionary, it is unique, return true.
-    HashMap<String, Set<String>> hm = new HashMap<String, Set<String>>();
+    // If the word corresponds to a key, and the word is the only one that have the same key, return true.
+    // If the key of the word does not belong to the keys of the dictionary, return true.
+    private HashMap<String, Set<String>> hm = new HashMap<String, Set<String>>();
+    
     public ValidWordAbbr(String[] dictionary) {
-        for (int i = 0; i < dictionary.length; i++) {
+        for (int i = 0; i < dictionary.length; ++i) {
             String key = getAbbreviation(dictionary[i]);
-            if (hm.containsKey(key)) hm.get(key).add(dictionary[i]);
-            else {
+            if (hm.containsKey(key)) {
+                hm.get(key).add(dictionary[i]);
+            } else {
                 Set<String> ss = new HashSet<String>();
                 ss.add(dictionary[i]);
                 hm.put(key, ss);
@@ -26,21 +27,18 @@ public class ValidWordAbbr {
         return false;
     }
     
-    public String getAbbreviation(String word) {
-        String key = null;
-        if (word.length() <= 2) key = word;
-        else {
+    private String getAbbreviation(String word) {
+        if (word.length() <= 2) {
+            return word;
+        } else {
             StringBuilder sb = new StringBuilder();
             sb.append(word.charAt(0));
             sb.append(word.length() - 2);
             sb.append(word.charAt(word.length() - 1));
-            key = new String(sb);            
+            return new String(sb);            
         }
-        return key;
     }
 }
-
-
 // Your ValidWordAbbr object will be instantiated and called as such:
 // ValidWordAbbr vwa = new ValidWordAbbr(dictionary);
 // vwa.isUnique("Word");
