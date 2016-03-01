f66501860a791ad8e39c734651bc52698b309c01
douglasleer <douglasleer@gmail.com>
2016-03-01 13:33:37 -0500

    Update 287.find-the-duplicate-number.java

--- 287.find-the-duplicate-number.java
+++ 287.find-the-duplicate-number.java
@@ -4,17 +4,17 @@
 
 public class Solution {
     public int findDuplicate(int[] nums) {
-        if (nums == null || nums.length == 0) return 1;
+        if (nums == null) throw new NullPointerException();
+        if (nums.length == 0) return -1;
         int low = 1, high = nums.length - 1;
         int n = nums.length - 1;
         while (true) {
             if (low == high) break;
-            int mid = low + ((high - low)>>1);
+            int mid = low + ((high - low) >> 1);
             int count = 0;
             for (int i = 0; i < nums.length; ++i) {
                 if (nums[i] > mid) ++count;
             }
-            
             if (count >= n + 1 - mid) low = mid + 1;
             else high = mid;
         }
