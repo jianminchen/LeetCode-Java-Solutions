345e315f832388fd224e6a2902adb40fd379d0af
douglasleer <douglasleer@gmail.com>
2016-03-01 07:57:47 -0500

    Update 261.graph-valid-tree.java

--- 261.graph-valid-tree.java
+++ 261.graph-valid-tree.java
@@ -1,7 +1,7 @@
 /**
  * @see <a href="https://leetcode.com/problems/graph-valid-tree/">Graph Valid Tree</a>
  */
-
+// using a queue for Breadth-First Search, the queue stores the edges, instead of vertices.
 public class Solution {
     public boolean validTree(int n, int[][] edges) {
         List<List<Integer>> listNeighbor = getListNeighbors(n, edges); // use function to simplify coding.
@@ -15,8 +15,9 @@ public class Solution {
         while (!q.isEmpty()) {
             int[] edge = q.remove();
             int start = edge[0], end = edge[1];
-            if (visited[end] == true) return false;
-            else {
+            if (visited[end] == true) {
+                return false;
+            } else {
                 visited[end] = true;
                 ++visitedCount;
                 for (int i : listNeighbor.get(end)) {
@@ -26,18 +27,16 @@ public class Solution {
                 }
             }
         }
-        
         return visitedCount == n;
     }
-    public List<List<Integer>> getListNeighbors(int n, int[][] edges) {
+    
+    private List<List<Integer>> getListNeighbors(int n, int[][] edges) {
         List<List<Integer>> listNeighbor = new ArrayList<List<Integer>>();
         for (int i = 0; i < n; ++i) {
-            List<Integer> neighbors = new ArrayList<>();
-            listNeighbor.add(neighbors);
+            listNeighbor.add(new ArrayList<>());
         }
         for (int i = 0; i < edges.length; ++i) {
-            int start = edges[i][0];
-            int end = edges[i][1];
+            int start = edges[i][0], end = edges[i][1];
             listNeighbor.get(start).add(end);
             listNeighbor.get(end).add(start);
         }
