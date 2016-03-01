283565d6ec786ac43e93aa6af797955a851c3f17
douglasleer <douglasleer@gmail.com>
2016-03-01 02:30:15 -0500

    Update 033.search-in-rotated-sorted-array.java

--- 033.search-in-rotated-sorted-array.java
+++ 033.search-in-rotated-sorted-array.java
@@ -2,45 +2,23 @@
  * @see <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/">Search in Rotated Sorted Array</a>
  */
 
+// Binary search solution: by comparing nums[mid] to nums[high], we can determine which condition we are in;
+// In different conditions, we can further determine which side the target can be by comparing target 
+// with nums[mid], nums[low], and nums[high]. We can combine some of the conditions to make the code simple and clean.
 public class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        int low = 0; int high = nums.length - 1;
+        int low = 0, high = nums.length - 1, mid = -1;
         while (true) {
-            if (low == high) {
-                if (target == nums[low]) return low;
-                else return -1;
-            }
-            if (low == high - 1) {
-                if (target == nums[low]) return low;
-                else if (target == nums[high]) return high;
-                else return -1;
-            }
-            int mid = low + ((high - low) >> 1);
-            
-            if (target > nums[low]) {
-                // the first part.
-                if (nums[mid] > nums[low]) {
-                    if (target < nums[mid]) high = mid -1;
-                    else if (target == nums[mid]) return mid;
-                    else low = mid + 1;
-                } else {// since mid != low, we have nums[mid] != nums[low]
-                    // nums[mid] < nums[low]
-                    high = mid - 1;
-                }
-            } else if (target == nums[low]) {
-                return low;
+            if (low > high) return -1;
+            mid = low + ((high - low) >> 1);
+            if (target == nums[mid]) break;
+            if ((nums[mid] > nums[high] && target < nums[mid] && target >= nums[low])
+                    || (nums[mid] < nums[high] && (target > nums[high] || target < nums[mid]))) {
+                high = mid - 1;
             } else {
-                // target < nums[low], the second part ? 
-                if (nums[mid] > nums[low]) {
-                    low = mid + 1;
-                } else {
-                    // nums[mid] < nums[low]
-                    if (target > nums[mid]) low = mid + 1;
-                    else if (target == nums[mid]) return mid;
-                    else high = mid - 1;
-                }
+                low = mid + 1;
             }
         }
+        return mid;
     }
 }
