a86f39c7fad8dfdc99c1c24ebc0730c4c9a7b5f4
douglasleer <douglasleer@gmail.com>
2016-03-01 13:07:24 -0500

    Update 216.combination-sum-iii.java

--- 216.combination-sum-iii.java
+++ 216.combination-sum-iii.java
@@ -3,10 +3,9 @@
  */
 
 public class Solution {
-    public int sum;
-    public Stack<Integer> stk;
-    public int n;
-    public int k;
+    // explicit iterative backtracking solution using stack.
+    private Stack<Integer> stk;
+    private int sum, n, k;
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         stk = new Stack<Integer>();
@@ -18,53 +17,37 @@ public class Solution {
             stk.push(i);
             sum += i;
         }
-
         // we use a stack that has k - 1 values, to represent a solution, the last number will be n - sum
         while (true) {
             int last = n - sum;
             if (last > 9) {
                 stk.push(stk.pop() + 1);
                 sum += 1;
-            }
-            // backtrack go to next availabe state if possible, and set stk to empty, if not possible.
-            else if (last <= stk.peek()) backtrack(); 
-            else {
-                // we have a solution here; thus, we find the solution and go to next state.
-                List<Integer> list = getResultGivenStack(); 
-                res.add(list);
+            } else if (last <= stk.peek()) {
+                backtrack(); // backtrack go to next availabe state if possible, and set stk to empty if not possible.
+            } else { // we have a solution here; thus, we find the solution and go to next state.
+                List<Integer> aRes = new ArrayList<>(stk);
+                aRes.add(n - sum);
+                res.add(aRes);
                 stk.push(stk.pop() + 1);
-                sum = sum + 1;                
+                sum = sum + 1;
             }
             if (stk.isEmpty()) break;            
         }
         return res;
     }
     
-    public List<Integer> getResultGivenStack() {
-        ArrayList<Integer> aList = new ArrayList<Integer>();
-        while (!stk.isEmpty()) {
-            aList.add(stk.pop());
-        }
-        Collections.reverse(aList);
-        for (int i = 0; i < aList.size(); ++i) {
-            stk.push(aList.get(i));
-        }
-        aList.add(n - sum);
-        return aList;
-    }
-    
-    public void backtrack() {
-        int popCount = 0;
-        int minN = 0;
+    private void backtrack() {
+        int popCount = 0, minN = 0;
         do {
             int top = stk.pop();
             ++popCount;
             sum = sum - top;
-            if (stk.isEmpty()) return;
+            if (stk.isEmpty()) return; // cannot backtrack
             
             stk.push(stk.pop() + 1);
             sum = sum + 1;
-            minN = sum; 
+            minN = sum;
             for (int i = 0; i < k - stk.size(); ++i) {
                 minN += stk.peek() + 1 + i;
             }
