9519849014ec56dbb41e53e16a67a4ca8b3ac305
douglasleer <douglasleer@gmail.com>
2016-03-01 05:03:33 -0500

    Update 300.longest-increasing-subsequence.java

--- 300.longest-increasing-subsequence.java
+++ 300.longest-increasing-subsequence.java
@@ -3,23 +3,23 @@
  */
 
 public class Solution {
+    // dynamic programming solution
     public int lengthOfLIS(int[] nums) {
-        // dynamic programming.
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null) throw new NullPointerException();
+        if (nums.length <= 1) return nums.length;
+        
+        // maxLen[i] will be the max len of a subsequence that start at i.
         int maxLen[] = new int[nums.length];
         for (int i = 0; i < maxLen.length; ++i) {
             maxLen[i] = 1;
         }
+        int max = maxLen[0];
         for (int i = maxLen.length - 2; i >= 0; --i) {
             for (int j = i + 1; j < maxLen.length; ++j) {
                 if (nums[i] < nums[j]) {
                     maxLen[i] = Math.max(maxLen[i], 1 + maxLen[j]);
                 }
             }
-        }
-        // maxLen[i] will be the max len of a subsequence that start at i.
-        int max = maxLen[0];
-        for (int i = 1; i < maxLen.length; ++i) {
             if (max < maxLen[i]) max = maxLen[i];
         }
         return max;
