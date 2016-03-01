f8ba11d9b4acb7ea62227bbcce60226515d8e332
douglasleer <douglasleer@gmail.com>
2016-03-01 15:45:42 -0500

    Update 269.alien-dictionary.java

--- 269.alien-dictionary.java
+++ 269.alien-dictionary.java
@@ -1,53 +1,53 @@
 /**
  * @see <a href="https://leetcode.com/problems/alien-dictionary/">Alien Dictionary</a>
  */
+// after getting the precedence order of the characters, the problem becomes the same as the classic topological sorting problem
+// and then is similar to Course Schedule, and Course Schedule II. We can use a dependent list, or dependent set, and 
+// an indegree count array/map to generate a topological sorting.
 
 public class Solution {
     public String alienOrder(String[] words) {
         Map<Character, Set<Character>> dependList = new HashMap<>();
+        Map<Character, Integer> inDegreeMap = new HashMap<>();
         for (int i = 0; i < words.length; ++i) {
             for (int j = 0; j < words[i].length(); ++j) {
                 if (!dependList.containsKey(words[i].charAt(j))) {
-                    Set<Character> s = new HashSet<>();
-                    dependList.put(words[i].charAt(j), s);
+                    // add an entry in the maps for character that appear at least once
+                    dependList.put(words[i].charAt(j), new HashSet<>());
+                    inDegreeMap.put(words[i].charAt(j), 0);
                 }
             }
         }
-        
-        for (int i = 1; i < words.length; ++i) {
-            String prev = words[i - 1];
-            String cur = words[i];
-            // get the precedence of the chars from these two strings.
+        for (int i = 1; i < words.length; ++i) { // get the precedence of the chars from two strings.
+            String prev = words[i - 1], cur = words[i];
             int j = 0;
             while (j < prev.length() && j < cur.length() && prev.charAt(j) == cur.charAt(j)) ++j;
-            if (j == prev.length() || j == cur.length()) ; // can't get info
+            if (j == prev.length() || j == cur.length()) {} // can't get info
             else {
-                dependList.get(cur.charAt(j)).add(prev.charAt(j));
-            }
-        }
-        
-        
-        StringBuilder sb = new StringBuilder();
-        while (!dependList.isEmpty()) {
-            Set<Character> charSet = dependList.keySet(); // consider all chars in the remaining dependList
-            List<Character> charToRemove = new ArrayList<>();
-            for (char c : charSet) { // concurrent modification exception ????
-                if (dependList.get(c).isEmpty()) {
-                    charToRemove.add(c);
-                    sb.append(c);
-                    for (char cc : charSet) { // update all dependLists.
-                        if (dependList.get(cc).contains(c)) {
-                            dependList.get(cc).remove(c);
-                        }
-                    }
+                if (!dependList.get(prev.charAt(j)).contains(cur.charAt(j))) {
+                    dependList.get(prev.charAt(j)).add(cur.charAt(j));
+                    // when there are duplicates, we can't add 1! So, inside the if
+                    inDegreeMap.put(cur.charAt(j), inDegreeMap.get(cur.charAt(j)) + 1);
                 }
             }
-            
-            for (char c : charToRemove) {
-                dependList.remove(c);
-            }
-            if (charToRemove.size() == 0) return "";
         }
-        return new String(sb);
+        
+        Queue<Character> q = new LinkedList<>(); // use a queue to do Breadth-First Search
+        for (char c : inDegreeMap.keySet()) {
+            if (inDegreeMap.get(c) == 0) q.add(c); // add chars that do not depend on any char first
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            char c = q.remove();
+            sb.append(c);
+            for (char dependent : dependList.get(c)) {
+                inDegreeMap.put(dependent, inDegreeMap.get(dependent) - 1);
+                if (inDegreeMap.get(dependent) == 0) {
+                    q.add(dependent);
+                }
+            }
+        }
+        if (inDegreeMap.size() == sb.length()) return new String(sb);
+        else return "";
     }
 }
