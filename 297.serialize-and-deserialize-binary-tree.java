e35d3932dd3e02165ab71e0e285e778a1d0dc555
douglasleer <douglasleer@gmail.com>
2016-03-01 04:43:55 -0500

    Update 297.serialize-and-deserialize-binary-tree.java

--- 297.serialize-and-deserialize-binary-tree.java
+++ 297.serialize-and-deserialize-binary-tree.java
@@ -11,53 +11,43 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+// use an array to store a tree node;
+// array[0] = value;
+// array[1] = li, // left child index, if null, -1
+// array[2] = ri, // right child index, if null, -1
 public class Codec {
+    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        // nodes will be added to an arrayList of arrays; then, convert the arraylist to a string.
         if (root == null) return new String();
         List<int[]> list = new ArrayList<int[]>();
         Stack<TreeNode> stk = new Stack<TreeNode>();
         Stack<Integer> iStk = new Stack<Integer>();
         TreeNode cur = root;
         int ci = 0;
-        while (true) {
+        while (true) { // preorder traversal to construct the arraylist
             while (cur != null) {
-                if (stk.isEmpty()) {
-                    int[] first = new int[3];
-                    first[0] = cur.val;
-                    first[1] = -1;
-                    first[2] = -1;
-                    list.add(first);
-                }
-                else { // we need to get the parent, and set the left and right of the parent.
-                    int pi = iStk.peek();
-                    // cur index is ci.
+                if (!stk.isEmpty()) { // get the parent, and set the left and right of the parent.
+                    int pi = iStk.peek(); // ci is the current index
                     if (cur == stk.peek().left) { // cur is left child of the parent
                         list.get(pi)[1] = ci;
-                    }
-                    else { // cur is the right child of the parent
+                    } else { // cur is the right child of the parent
                         list.get(pi)[2] = ci;
                     }
-                    int[] curNode = new int[3];
-                    curNode[0] = cur.val;
-                    curNode[1] = -1;
-                    curNode[2] = -1;
-                    list.add(curNode);
                 }
+                list.add(new int[]{cur.val, -1, -1});
                 stk.push(cur);
                 iStk.push(ci);
                 cur = cur.left;
                 ++ci;
             }
-            if (stk.isEmpty()) break;
-            if (stk.peek().right != null) cur = stk.peek().right;
-            else {
-                do {
-                    cur = stk.pop();
-                    iStk.pop();
-                } while (!stk.isEmpty() && (stk.peek().right == null || stk.peek().right == cur));
-                if (stk.isEmpty()) break;
-                else cur = stk.peek().right;
+            while (!stk.isEmpty() && (stk.peek().right == null || stk.peek().right == cur)) {
+                cur = stk.pop();
+                iStk.pop();
             }
+            if (stk.isEmpty()) break;
+            else cur = stk.peek().right;
         }
         
         StringBuilder sb = new StringBuilder();
@@ -72,11 +62,10 @@ public class Codec {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        // convert the received string into the ArrayList,
-        // then, reconstruct the tree using the arraylist.
+        // convert the string into an arrayList; then, reconstruct the tree using the arraylist.
         if (data.length() == 0) return null;
         List<Integer> li = new ArrayList<Integer>();
-        int i = 0; 
+        int i = 0;
         while (i < data.length()) {
             int si = i;
             while (data.charAt(i) != ' ') ++i;
@@ -92,26 +81,18 @@ public class Codec {
             }
             list.add(node);
         }
-        
         // reconstruct the tree using the list of arrays.
         TreeNode root = generateTree(list, 0);
         return root;
     }
     
-    public TreeNode generateTree(List<int[]> list, int index) {
+    private TreeNode generateTree(List<int[]> list, int index) {
         TreeNode node = new TreeNode(list.get(index)[0]);
-        if (list.get(index)[1] == -1) node.left = null;
-        else {
-            node.left = generateTree(list, list.get(index)[1]);
-        }
-        if (list.get(index)[2] == -1) node.right = null;
-        else {
-            node.right = generateTree(list, list.get(index)[2]);
-        }
+        node.left = list.get(index)[1] == -1 ? null : generateTree(list, list.get(index)[1]);
+        node.right = list.get(index)[2] == -1 ? null: generateTree(list, list.get(index)[2]);
         return node;
     }
 }
-
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
