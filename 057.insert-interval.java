07dbaab9bfb9020d9d233e2d57289403a13509cd
douglasleer <douglasleer@gmail.com>
2016-03-01 03:01:54 -0500

    Update 057.insert-interval.java

--- 057.insert-interval.java
+++ 057.insert-interval.java
@@ -11,84 +11,42 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
 public class Solution {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-
-        Iterator<Interval> iter = intervals.iterator();
-        List<Interval> newInts = new ArrayList<>();
-        
-        if (intervals.size() == 0) {
-            newInts.add(newInterval);
-            return newInts;
-        }
-        
-        Interval cur = null;
-        while (iter.hasNext()) {
-            cur = iter.next();
-            if (newInterval.start > cur.end) {
-                newInts.add(cur);   
+        if (intervals == null || newInterval == null) throw new NullPointerException();
+        List<Interval> result = new ArrayList<>();
+        boolean newIntervalAdded = false;
+        for (int i = 0; i < intervals.size(); ++i) {
+            if (!newIntervalAdded && intervals.get(i).start > newInterval.start) {
+                addInterval(result, newInterval);
+                newIntervalAdded = true;
+                --i; // we need to reconsider the current interval
+            } else {
+                addInterval(result, intervals.get(i));
             }
-            else break;
         }
-        if (!iter.hasNext()) {
-            if (newInterval.end < cur.start) {
-                newInts.add(newInterval);
-                newInts.add(cur);
-                return newInts;
-            }
-            else if (newInterval.end <= cur.end) {
-                if (newInterval.start < cur.start) {
-                    newInts.add(new Interval(newInterval.start, cur.end));
-                }
-                else {
-                    newInts.add(new Interval(cur.start, cur.end));
-                }
-                return newInts;
-            }
-            else { // newInterval.end > cur.end;
-                if (newInterval.start > cur.end) {
-                    newInts.add(newInterval);
-                    return newInts;
-                }
-                else { // newInterval.start <= cur.end
-                    newInts.add(new Interval(Math.min(newInterval.start, cur.start), newInterval.end));
-                    return newInts;
-                }
-            }
-
+        if (!newIntervalAdded) { // if the newInterval haven't been added, we add it at last
+            addInterval(result, newInterval);
         }
-        
-        int newStart = 0, newEnd = 0;
-        if (newInterval.start < cur.start) newStart = newInterval.start;
-        else newStart = cur.start;
-        
-        do {
-            if (newInterval.end > cur.end && iter.hasNext()) {
-                cur = iter.next();
-            }
-            else {
-                break;
-            }
-        } while (iter.hasNext());
-        
-        if (newInterval.end < cur.start) {
-            newEnd = newInterval.end;
-            newInts.add(new Interval(newStart, newEnd));
-            newInts.add(cur);
+        return result;
+    }
+    
+    // the newInterval has a starting time that is greater than the last interval's starting time in intervals
+    private void addInterval(List<Interval> intervals, Interval newInterval) {
+        if (intervals.size() == 0 || newInterval.start > intervals.get(intervals.size() - 1).end) {
+            intervals.add(newInterval);
+        } else {
+            intervals.get(intervals.size() - 1).end = 
+                    Math.max(intervals.get(intervals.size() - 1).end, newInterval.end);
         }
-        else if (newInterval.end >= cur.end) {
-            newEnd = newInterval.end;
-            newInts.add(new Interval(newStart, newEnd));
-            // covers cur.
-        }
-        else { // newInterval.end >= cur.start, < cur.end;
-            newEnd = cur.end;
-            newInts.add(new Interval(newStart, newEnd));
-        }
-        
-        while (iter.hasNext()) {
-            newInts.add(iter.next());
-        }
-        return newInts;
     }
 }
