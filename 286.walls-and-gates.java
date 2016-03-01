f9a135fd24c48d44feaf9ab8c45aa319b479d8ef
douglasleer <douglasleer@gmail.com>
2016-03-01 05:53:02 -0500

    Update 286.walls-and-gates.java

--- 286.walls-and-gates.java
+++ 286.walls-and-gates.java
@@ -4,38 +4,30 @@
 
 public class Solution {
     public void wallsAndGates(int[][] rooms) {
+        if (rooms == null) throw new NullPointerException();
         if (rooms.length == 0 || rooms[0].length == 0) return;
         Queue<int[]> q = new LinkedList<>();
-        
-        for (int i = 0; i < rooms.length; ++i) {
-            for (int j = 0; j < rooms[0].length; ++j) {
+        int row = rooms.length, col = rooms[0].length;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
                 if (rooms[i][j] == 0) {
-                    int[] zeroLoc = new int[]{i, j};
-                    q.add(zeroLoc);
+                    q.add(new int[]{i, j});
                 }
             }
         }
-
+        
+        // use some variables can help to write clean code!
+        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         while (!q.isEmpty()) {
             int[] zLoc = q.remove();
             int i = zLoc[0], j = zLoc[1];
-            if (i - 1 >= 0 && rooms[i - 1][j] == Integer.MAX_VALUE) {
-                q.add(new int[]{i - 1, j});
-                rooms[i - 1][j] = rooms[i][j] + 1;
-            }
-            if (j + 1 <= rooms[0].length - 1 && rooms[i][j + 1] == Integer.MAX_VALUE) {
-                q.add(new int[]{i, j + 1});
-                rooms[i][j + 1] = rooms[i][j] + 1;
-            }
-            if (i + 1 <= rooms.length - 1 && rooms[i + 1][j] == Integer.MAX_VALUE) {
-                q.add(new int[]{i + 1, j});
-                rooms[i + 1][j] = rooms[i][j] + 1;
-            }
-            if (j - 1 >= 0 && rooms[i][j - 1] == Integer.MAX_VALUE) {
-                q.add(new int[]{i, j - 1});
-                rooms[i][j - 1] =  rooms[i][j] + 1;
+            for (int[] d : dir) { // basically, check the neighbors of the current position
+                if (i + d[0] >= 0 && i + d[0] < row && j + d[1] >= 0 && j + d[1] < col
+                        && rooms[i + d[0]][j + d[1]] == Integer.MAX_VALUE) {
+                    q.add(new int[]{i + d[0], j + d[1]});
+                    rooms[i + d[0]][j + d[1]] = rooms[i][j] + 1;
+                }
             }
         }
-        
     }
 }
