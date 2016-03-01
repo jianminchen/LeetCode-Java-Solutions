75704664cca7fb68870ccda2b9a3b76568abd62b
douglasleer <douglasleer@gmail.com>
2016-03-01 04:18:05 -0500

    Update 294.flip-game-ii.java

--- 294.flip-game-ii.java
+++ 294.flip-game-ii.java
@@ -4,15 +4,16 @@
 
 public class Solution {
     public boolean canWin(String s) {
-        List<String> list = generateAll(s);
+        // generate all possible strings after a move. 
+        // This seems to have repeated calculations. TODO: try to use memorization technique to reduce time complexity
+        List<String> list = generateAll(s); 
         if (list.isEmpty()) return false;
-        boolean canW = false;
         for(String aStr : list) {
-            canW = canW || !canWin(aStr);
+            if (!canWin(aStr)) return true; // if the other one is to lose, we can win
         }
-        return canW;
+        return false;
     }
-    public List<String> generateAll(String s) {
+    private List<String> generateAll(String s) {
         List<String> list = new ArrayList<String>();
         int i = 0;
         while (i < s.length()) {
@@ -24,18 +25,16 @@ public class Solution {
             int startPlus = i;
             while (i < s.length() && s.charAt(i) == '+') ++i;
             int endPlus = i;
-            if (startPlus == endPlus - 1) ;// no possible move
-            else {
-                // give startPlus, and endPlus, generate all possible strings after one move.
+            if (startPlus == endPlus - 1) {}
+            else { // give startPlus, and endPlus, generate all possible strings after one move.
                 List<String> subList = generateGivenStartAndEnd(s, startPlus, endPlus);
                 list.addAll(subList);
             }
         }
         return list;
     }
-    public List<String> generateGivenStartAndEnd(String s, int startPlus, int endPlus) {
-        // endPlus is exclusive.
-        List<String> list = new ArrayList<String>();
+    private List<String> generateGivenStartAndEnd(String s, int startPlus, int endPlus) {
+        List<String> list = new ArrayList<String>();  // endPlus is exclusive.
         for (int i = startPlus; i <= endPlus - 2; ++i) {
             String newS = s.substring(0, i) + "--" + s.substring(i + 2);
             list.add(newS);
