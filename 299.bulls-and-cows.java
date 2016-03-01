769aa022eac197ff18d3f4078e1cd7a5080fbc6c
douglasleer <douglasleer@gmail.com>
2016-03-01 04:56:43 -0500

    Update 299.bulls-and-cows.java

--- 299.bulls-and-cows.java
+++ 299.bulls-and-cows.java
@@ -5,38 +5,30 @@
 public class Solution {
     public String getHint(String secret, String guess) {        
         int correct = 0;
-        Map<Character, Integer> hm1 = new HashMap<>();
-        Map<Character, Integer> hm2 = new HashMap<>();
+        Map<Character, Integer> hm1 = new HashMap<>(), hm2 = new HashMap<>();
         
         for (int i = 0; i < secret.length(); ++i) {
             if (secret.charAt(i) == guess.charAt(i)) {
-                correct ++;
-            }
-            else {
-                if (hm1.containsKey(secret.charAt(i))) {
-                    hm1.put(secret.charAt(i), hm1.get(secret.charAt(i)) + 1);
-                }
-                else hm1.put(secret.charAt(i), 1);
-                if (hm2.containsKey(guess.charAt(i))) {
-                    hm2.put(guess.charAt(i), hm2.get(guess.charAt(i)) + 1);
-                }
-                else hm2.put(guess.charAt(i), 1);
+                ++correct;
+            } else {
+                char c = secret.charAt(i);
+                hm1.put(c, hm1.containsKey(c) ? hm1.get(c) + 1 : 1);
+                c = guess.charAt(i);
+                hm2.put(c, hm2.containsKey(c) ? hm2.get(c) + 1 : 1);
             }
         }
+        
         StringBuilder sb = new StringBuilder();
         sb.append(correct);
         sb.append('A');
         int cows = 0;
-        Set<Character> guessSet = hm2.keySet();
-        for (Character c : guessSet) {
+        for (char c : hm2.keySet()) {
             if (hm1.containsKey(c)) {
                 cows += Math.min(hm1.get(c), hm2.get(c));
             }
         }
-        
         sb.append(cows);
         sb.append('B');
-        
         return new String(sb);
     }
 }
