e180315b726392077852231c93eaaddd4e5e663c
douglasleer <douglasleer@gmail.com>
2016-03-01 07:41:10 -0500

    Update 166.fraction-to-recurring-decimal.java

--- 166.fraction-to-recurring-decimal.java
+++ 166.fraction-to-recurring-decimal.java
@@ -7,11 +7,10 @@ public class Solution {
         if (denominator == 0) {
             throw new IllegalArgumentException(" denominator can not be zero");
         }
-        long longN = numerator;
-        long longD = denominator;
-        return myFractionToDecimal(longN, longD);
+        // use long type to avoid integer overflow
+        return myFractionToDecimal((long) numerator, (long) denominator);
     }
-    public String myFractionToDecimal(long numerator, long denominator) {
+    private String myFractionToDecimal(long numerator, long denominator) {
         if (numerator == 0) return "0";
         if (numerator < 0 && denominator > 0) {
             return new String("-" + myFractionToDecimal(-1 * numerator, denominator));
@@ -28,45 +27,40 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
         sb.append(iPart);
         if (remainder == 0) return new String(sb);
-        // 1 / 7
+
         sb.append(".");
         // if a remainder starts repeating once. the fraction will re-occur from now on.
         List<Long> fraction = new ArrayList<Long>(); // arraylist stores long
         Map<Long, Integer> hm = new HashMap<Long, Integer>(); // map stores long and integer.
-        int repeatIndex = -1;
+        int repeatIndex = -1; // where the recurring starts
         while (remainder != 0) {
             if (hm.containsKey(remainder)) {
                 repeatIndex = hm.get(remainder);
                 break;
-            }
-            else {
+            } else {
                 hm.put(remainder, fraction.size()); // the digits for this remainder starts here.
                 while (remainder * 10 < denominator) {
                     remainder *= 10;
                     fraction.add(0l);
                     if (!hm.containsKey(remainder)) {
                         hm.put(remainder, fraction.size());
-                    }
-                    else {
+                    } else {
                         repeatIndex = hm.get(remainder);
                         break; // recurring starts here, jump out
                     }
-
                 }
                 if (repeatIndex != -1) break; // recurring detected, jump out.
-                
                 long digit = remainder * 10 / denominator;
                 fraction.add(digit);
                 remainder = remainder * 10 % denominator;
             }
         }
         
-        if (repeatIndex == -1) { // no re-occurring tails.
+        if (repeatIndex == -1) { // no reccurring tails.
             for (int i = 0; i < fraction.size(); ++i) {
                 sb.append(fraction.get(i));
             }
-        }
-        else {
+        } else { // with recurring tails, need to add parentheses
             for (int i = 0; i < repeatIndex; ++i) {
                 sb.append(fraction.get(i));
             }
