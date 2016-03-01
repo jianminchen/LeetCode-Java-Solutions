a9d54d8b811921b047525c7fdd8c01ad52431156
douglasleer <douglasleer@gmail.com>
2016-03-01 03:23:33 -0500

    Update 044.wildcard-matching.java

--- 044.wildcard-matching.java
+++ 044.wildcard-matching.java
@@ -4,6 +4,7 @@
 
 public class Solution {
     public boolean isMatch(String s, String p) {
+        if (s == null || p == null) throw new NullPointerException();
         boolean[][] match = new boolean[s.length() + 1][p.length() + 1];
         match[s.length()][p.length()] = true;
         int j = p.length() - 1;
@@ -13,12 +14,14 @@ public class Solution {
         }
         for (int i = s.length() - 1; i >= 0; --i) {
             for (int k = p.length() - 1; k >= 0; --k) {
-                if (p.charAt(k) == '?' || p.charAt(k) == s.charAt(i)) 
-                    match[i][k] = match[i + 1][k + 1];
-                else if (p.charAt(k) == '*')
+                if (p.charAt(k) == '?' || p.charAt(k) == s.charAt(i)) {
+                    match[i][k] = match[i + 1][k + 1];                    
+                } else if (p.charAt(k) == '*') {
                     match[i][k] = match[i + 1][k] || match[i][k + 1];
-                else match[i][k] = false;
-            } 
+                } else {
+                    match[i][k] = false;
+                }
+            }
         }
         return match[0][0];
     }
