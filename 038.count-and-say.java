37e4451a9c64aa69dff0a60e8b35fb9d37ce43a6
douglasleer <douglasleer@gmail.com>
2016-03-02 17:46:43 -0500

    Update 038.count-and-say.java

--- 038.count-and-say.java
+++ 038.count-and-say.java
@@ -6,14 +6,14 @@ public class Solution {
     public String countAndSay(int n) {
         if (n <= 0) return "";
         String cur = "1";
-        for (int t = 2; t <= n; t ++) {
+        for (int t = 2; t <= n; ++t) {
             StringBuilder sbNext = new StringBuilder();
             int i = 0;
             while (i < cur.length()) {
                 int count = 1;
                 int j = i + 1;
-                for (; j < cur.length() && cur.charAt(j) == cur.charAt(j - 1); j ++) {
-                    count ++;
+                for (; j < cur.length() && cur.charAt(j) == cur.charAt(j - 1); ++j) {
+                    ++count;
                 }
                 sbNext.append(count);
                 sbNext.append(cur.charAt(i));
