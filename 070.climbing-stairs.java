9cdf0ec5a1ec1b6cd9385489424762bb00b4dcc9
douglasleer <douglasleer@gmail.com>
2016-03-02 18:14:26 -0500

    Update 070.climbing-stairs.java

--- 070.climbing-stairs.java
+++ 070.climbing-stairs.java
@@ -4,12 +4,11 @@
 
 public class Solution {
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        if (n == 1 || n == 2) return n;
         int[] steps = new int[n]; // the memory complexity can be reduced to O(1)
         steps[0] = 1;
         steps[1] = 2;
-        for (int i = 2; i < n; i ++) {
+        for (int i = 2; i < n; ++i) {
             steps[i] = steps[i-1] + steps[i-2];
         }
         return steps[n-1];
