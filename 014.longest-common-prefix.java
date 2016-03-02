2a3a0d7dc1cfe5092753ef206c9f3f44cea6cb68
douglasleer <douglasleer@gmail.com>
2016-03-02 16:48:57 -0500

    Update 014.longest-common-prefix.java

--- 014.longest-common-prefix.java
+++ 014.longest-common-prefix.java
@@ -4,7 +4,8 @@
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return ""; 
+        if (strs == null) throw new NullPointerException();
+        if (strs.length == 0) return ""; 
         for (int i = 0; i < strs.length; i ++) {
             if (strs[i] == null) return "";
         }
