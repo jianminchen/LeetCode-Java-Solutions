a433a0fa478b637dde0f00e43a3447950a3b4d85
douglasleer <douglasleer@gmail.com>
2016-03-02 18:42:31 -0500

    Update 079.word-search.java

--- 079.word-search.java
+++ 079.word-search.java
@@ -4,8 +4,8 @@
 
 public class Solution {
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
                 if(dfsHelper(board, i, j, word, 0)) return true;
             }
         }
