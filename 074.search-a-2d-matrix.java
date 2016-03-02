cd3c88f7af2dc159907e9c138ba7ac86b939906e
douglasleer <douglasleer@gmail.com>
2016-03-02 18:39:53 -0500

    Update 074.search-a-2d-matrix.java

--- 074.search-a-2d-matrix.java
+++ 074.search-a-2d-matrix.java
@@ -5,24 +5,23 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         // first get a possible candidate row.
+        if (matrix == null) throw new NullPointerException();
         if (matrix.length == 0 || matrix[0].length == 0) return false;
         int low = 0; int high = matrix.length - 1;
         int rCand = -1;
         while (true) {
             if (low > high) return false;
-
-            int mid = low + ((high - low)>>1);
-            if (target < matrix[mid][0]) high = mid - 1;
-            else if (target >= matrix[mid][0] && target <= matrix[mid][matrix[0].length - 1]) {
+            int mid = low + ((high - low) >> 1);
+            if (target < matrix[mid][0]) {
+                high = mid - 1;
+            } else if (target >= matrix[mid][0] && target <= matrix[mid][matrix[0].length - 1]) {
                 rCand = mid;
                 break;
-            }
-            else {
+            } else {
                 // target > matrix[mid][matrix[0].length - 1]
                 low = mid + 1;
             }
         }
-        
         // search for the target in the candidate row.
         low = 0; high = matrix[0].length - 1;
         while (true) {
