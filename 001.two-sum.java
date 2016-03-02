f20601f3233a1601d66fc0e320e21864a5bb3422
douglasleer <douglasleer@gmail.com>
2016-03-02 16:26:58 -0500

    Update 001.two-sum.java

--- 001.two-sum.java
+++ 001.two-sum.java
@@ -4,14 +4,14 @@
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        if (nums == null) throw new NullPointerException();
         int res[] = new int[2];
         Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
         hm.put(nums[0], 0);
         for (int i = 1; i < nums.length; ++i) {
             if (!hm.containsKey(target - nums[i])) {
                 hm.put(nums[i], i); 
-            }
-            else {
+            } else {
                 res[0] = hm.get(target - nums[i]) + 1;
                 res[1] = i + 1;
                 break;
