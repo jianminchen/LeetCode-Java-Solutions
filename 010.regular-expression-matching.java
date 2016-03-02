91ef30c0e93b6e82c951e3fa25e50ca53f1bcaf4
douglasleer <douglasleer@gmail.com>
2016-03-02 16:41:31 -0500

    Update 010.regular-expression-matching.java

--- 010.regular-expression-matching.java
+++ 010.regular-expression-matching.java
@@ -4,33 +4,33 @@
 
 public class Solution {
     public boolean isMatch(String s, String p) {
+        if (s == null || p == null) throw new NullPointerException();
         int sL = s.length(), pL = p.length();
         if (sL == 0 && pL == 0) return true;
         if (pL == 0 && sL != 0) return false;
-        else { // both length not equal to zero.
-            if (pL == 1) {
-                if (sL != 1) return false;
-                if (p.charAt(0) == '.') return true;
-                else return p.charAt(0) == s.charAt(0);
-            } else { // pL >= 2.
-                if (p.charAt(1) != '*') {
-                    if (sL == 0) return false;
-                    
-                    if (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) {
-                        if (isMatch(s.substring(1), p.substring(1))) return true;
+        
+        if (pL == 1) {
+            if (sL != 1) return false;
+            if (p.charAt(0) == '.') return true;
+            else return p.charAt(0) == s.charAt(0);
+        } else { // pL >= 2.
+            if (p.charAt(1) != '*') {
+                if (sL == 0) return false;
+                
+                if (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) {
+                    if (isMatch(s.substring(1), p.substring(1))) return true;
+                }
+            } else { // p.charAt(1) == '*'
+                if (isMatch(s, p.substring(2))) return true;
+                if (p.charAt(0) == '.') {
+                    for (int i = 1; i <= sL; ++i) {
+                        if (isMatch(s.substring(i), p.substring(2))) return true;
                     }
-                } else { // p.charAt(1) == '*'
-                    if (isMatch(s, p.substring(2))) return true;
-                    if (p.charAt(0) == '.') {
-                        for (int i = 1; i <= sL; ++i) {
-                            if (isMatch(s.substring(i), p.substring(2))) return true;
-                        }
-                    } else {
-                        int i = 0;
-                        while (i < sL && s.charAt(i) == p.charAt(0)) {
-                            if (isMatch(s.substring(i + 1), p.substring(2))) return true;
-                            ++i;
-                        }
+                } else {
+                    int i = 0;
+                    while (i < sL && s.charAt(i) == p.charAt(0)) {
+                        if (isMatch(s.substring(i + 1), p.substring(2))) return true;
+                        ++i;
                     }
                 }
             }
