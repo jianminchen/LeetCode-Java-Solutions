a4fb1157ceaa9d62bbce0654edc0781b14e3bc49
douglasleer <douglasleer@gmail.com>
2016-03-02 18:48:42 -0500

    Update 100.same-tree.java

--- 100.same-tree.java
+++ 100.same-tree.java
@@ -16,8 +16,9 @@ public class Solution {
         if (p == null && q == null) return true;
         if (p == null && q != null) return false;
         if (p != null && q == null) return false;
-        if (p.val != q.val) return false;
-        else {
+        if (p.val != q.val) {
+            return false;
+        } else {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
