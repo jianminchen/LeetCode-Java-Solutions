719d06f2ee4341836884daeca0f6d0f624033077
douglasleer <douglasleer@gmail.com>
2016-03-02 16:42:19 -0500

    Update 011.container-with-most-water.java

--- 011.container-with-most-water.java
+++ 011.container-with-most-water.java
@@ -4,9 +4,9 @@
 
 public class Solution {
     public int maxArea(int[] height) {
+        if (height == null) throw new NullPointerException();
         if (height.length == 0) return 0;
-        int i = 0;
-        int j = height.length - 1;
+        int i = 0, j = height.length - 1;
         int max = (j - i) * Math.min(height[i], height[j]);
         while (i < j) {
             if (height[i] >= height[j]) {
