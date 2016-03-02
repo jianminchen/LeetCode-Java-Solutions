02a4117bbe118d2a172e0ea6af5c8e380d7ecdaf
douglasleer <douglasleer@gmail.com>
2016-03-02 18:47:26 -0500

    Update 096.unique-binary-search-trees.java

--- 096.unique-binary-search-trees.java
+++ 096.unique-binary-search-trees.java
@@ -7,10 +7,10 @@ public class Solution {
         if (1 == n) return 1;
         int nums[] = new int[n];
         nums[0] = 1;
-        for (int i = 1; i <= n - 1; i ++) {
+        for (int i = 1; i <= n - 1; ++i) {
             int n1 = 2 * nums[i - 1]; // first as root and last as root;
             nums[i] = n1;
-            for (int j = 1; j <= i - 1; j ++) {
+            for (int j = 1; j <= i - 1; ++j) {
                 nums[i] += nums[j - 1] * nums[i - j - 1];
             }
         }
